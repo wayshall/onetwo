d1dc335f5e4c4826600d0d56f41daeba1bd0e2a8
wayshall <weishao.zeng@gmail.com>
2013-06-05 15:33:20 +0800

    create dq plugin project

---
+++ labs/onetwo-plugin-dq/src/main/java/org/onetwo/plugins/dq/DqPluginContext.java
@@ -0,0 +1,12 @@
+package org.onetwo.plugins.dq;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackageClasses=DqPlugin.class)
+public class DqPluginContext {
+	
+	
+}
