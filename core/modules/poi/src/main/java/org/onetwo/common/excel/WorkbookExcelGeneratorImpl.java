166f3bb2d2127bd81a4c8f4598d977a9fe5b9c47
wayshall <weishao.zeng@gmail.com>
2013-12-18 12:13:22 +0800

    add multip sheet supported

--- core/modules/poi/src/main/java/org/onetwo/common/excel/WorkbookExcelGeneratorImpl.java
+++ core/modules/poi/src/main/java/org/onetwo/common/excel/WorkbookExcelGeneratorImpl.java
@@ -1,28 +1,27 @@
 package org.onetwo.common.excel;
 
+import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.onetwo.common.interfaces.excel.ExcelValueParser;
 
 public class WorkbookExcelGeneratorImpl extends AbstractWorkbookExcelGenerator {
 	
 	private WorkbookModel workbookModel;
 	private Workbook workbook;
+	private Map<String, Object> context;
 
+	public WorkbookExcelGeneratorImpl(WorkbookModel workbookModel, Map<String, Object> context){
+		this.workbookModel = workbookModel;
+		this.context = context;
+	}
 	@Override
 	public void generateIt() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public ExcelValueParser getExcelValueParser() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setExcelValueParser(ExcelValueParser excelValueParser) {
-		// TODO Auto-generated method stub
-		
+		this.workbook = new HSSFWorkbook();
+		for(TemplateModel template : workbookModel.getSheets()){
+			PoiExcelGenerator pg = DefaultExcelGeneratorFactory.createExcelGenerator(workbook, template, context);
+			pg.generateIt();
+		}
 	}
 
 	@Override
