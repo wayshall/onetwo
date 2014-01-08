a4e7be04586dcc0cb6fec5e55ec1b7e0960ca62d
wayshall <weishao.zeng@gmail.com>
2014-01-08 10:34:05 +0800

    add batch command runner

--- labs/projects/batch/src/main/java/org/onetwo/project/batch/tools/ToolMain.java
+++ labs/projects/batch/src/main/java/org/onetwo/project/batch/tools/ToolMain.java
@@ -1,13 +1,8 @@
 package org.onetwo.project.batch.tools;
 
 import org.onetwo.common.log.MyLoggerFactory;
-import org.onetwo.common.spring.SpringApplication;
 import org.onetwo.common.spring.context.SpringConfigApplicationContext;
-import org.onetwo.common.utils.commandline.CmdRunner;
-import org.onetwo.common.utils.commandline.DefaultCommandManager;
-import org.onetwo.common.utils.commandline.ExitCommand;
-import org.onetwo.common.utils.commandline.HelpCommand;
-import org.onetwo.plugins.batch.cmd.JobCommand;
+import org.onetwo.plugins.batch.cmd.BatchCmdRunner;
 import org.onetwo.project.batch.BatchConfig;
 import org.slf4j.Logger;
 
@@ -20,28 +15,11 @@ public class ToolMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new CmdRunner(){
+		new BatchCmdRunner(){
 
-			protected void loadCommand(String[] args) {
-				cmdManager = new DefaultCommandManager();
-				cmdManager.addCommand(new ExitCommand());
-				cmdManager.addCommand(new HelpCommand());
-				cmdManager.addCommand(new JobCommand("exportPsamJob"));
-				cmdManager.addCommand(new JobCommand("importPsamJob"));
-			}
-			
-			@Override
-			protected void startAppContext(String[] args) {
-				//SpringProfilesWebApplicationContext
-//				SpringUtils.setProfiles(BatchConfig.getInstance().getAppEnvironment());
-//				ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-				SpringConfigApplicationContext context = new SpringConfigApplicationContext();
+			protected void initApplicationContext(SpringConfigApplicationContext context){
 				context.setAppEnvironment(BatchConfig.getInstance().getAppEnvironment());
 				context.register(ToolBatchContextConfig.class);
-//				context.setConfigLocation("");
-				context.refresh();
-				SpringApplication.initApplication(context);
-				
 			}
 			
 		}.run(args);
