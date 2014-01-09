c6143f06dd86d2a15543d8f07ec32ebee04e16c0
wayshall <weishao.zeng@gmail.com>
2014-01-09 16:26:19 +0800

    start....

--- core/modules/jfish/src/main/java/org/onetwo/common/excel/DatagridExcelModelBuilder.java
+++ core/modules/jfish/src/main/java/org/onetwo/common/excel/DatagridExcelModelBuilder.java
@@ -3,12 +3,20 @@ package org.onetwo.common.excel;
 import org.onetwo.common.web.view.jsp.datagrid.AbstractDatagridRenderListener;
 import org.onetwo.common.web.view.jsp.datagrid.DataGridTag;
 import org.onetwo.common.web.view.jsp.grid.GridTagBean;
+import org.onetwo.common.web.view.jsp.grid.RowTagBean;
 
 public class DatagridExcelModelBuilder extends AbstractDatagridRenderListener {
 
 	@Override
 	public void afterRender(DataGridTag tag, GridTagBean tagBean) {
 		WorkbookModel workbook = new WorkbookModel();
+		TemplateModel template = new TemplateModel();
+		template.setLabel(tagBean.getLabel());
+		template.setName(tagBean.getName());
+		for(RowTagBean rowTag : tagBean.getRows()){
+//			template.
+		}
+		workbook.addSheet(template);
 	}
 	
 
