100a5e7e7b9a0ae529e51fd1c350c1a1ea474a75
wayshall <weishao.zeng@gmail.com>
2014-01-10 15:46:32 +0800

    add job parameter builder for jobcommand

--- core/plugins/batch/src/main/java/org/onetwo/plugins/batch/cmd/BatchCmdRunner.java
+++ core/plugins/batch/src/main/java/org/onetwo/plugins/batch/cmd/BatchCmdRunner.java
@@ -5,6 +5,7 @@ import java.util.Map.Entry;
 
 import org.onetwo.common.cmd.SpringCmdRunner;
 import org.onetwo.common.spring.SpringApplication;
+import org.onetwo.common.utils.commandline.Command;
 import org.onetwo.common.utils.commandline.CommandManager;
 import org.springframework.batch.core.Job;
 
@@ -13,9 +14,13 @@ public class BatchCmdRunner extends SpringCmdRunner {
 	protected void loadUserCommand(CommandManager cmdManager){
 		Map<String, Job> jobs = SpringApplication.getInstance().getBeansMap(Job.class);
 		for(Entry<String, Job> job : jobs.entrySet()){
-			cmdManager.addCommand(new JobCommand(job.getKey()));
+			cmdManager.addCommand(createJobCommand(job.getKey(), job.getValue()));
 			logger.info("load JobCommand : {} ", job.getKey());
 		}
+	}
+	
+	protected Command createJobCommand(String jobBeanName, Job job){
+		return new JobCommand(jobBeanName);
 	}
 
 }
