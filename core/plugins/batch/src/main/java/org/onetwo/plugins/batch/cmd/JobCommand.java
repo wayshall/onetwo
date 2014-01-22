986668d1454c3bdc8339df2dbde5a02e57d1f939
wayshall <weishao.zeng@gmail.com>
2014-01-22 18:43:48 +0800

    modify jobcommand

--- core/plugins/batch/src/main/java/org/onetwo/plugins/batch/cmd/JobCommand.java
+++ core/plugins/batch/src/main/java/org/onetwo/plugins/batch/cmd/JobCommand.java
@@ -1,9 +1,12 @@
 package org.onetwo.plugins.batch.cmd;
 
+import java.io.IOException;
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
 import org.onetwo.common.log.MyLoggerFactory;
 import org.onetwo.common.spring.SpringApplication;
+import org.onetwo.common.utils.DateUtil;
 import org.onetwo.common.utils.commandline.AbstractCommand;
 import org.onetwo.common.utils.commandline.CmdContext;
 import org.slf4j.Logger;
@@ -37,8 +40,25 @@ public class JobCommand extends AbstractCommand {
 	}
 	
 	protected JobParameters buildJobParameter(CmdContext context) throws Exception {
+		String runTimeString = inputRunTime(context);
 		return new JobParametersBuilder()
-				.addDate("runDate", new Date())
+				.addString("runTime", runTimeString)
 				.toJobParameters();
+	}
+	
+	protected String inputRunTime(CmdContext context) throws IOException{
+		String runTimeString;
+		while(true){
+			System.out.print("输入任务的执行时间（ yyyy-MM-dd HH:mm:ss 当前时间回车即可）  > ");
+			runTimeString = context.getCmdInput().readLine();
+			if(StringUtils.isBlank(runTimeString)){
+				runTimeString = DateUtil.formatDateTime(new Date());
+			}else{
+				Date runTime = DateUtil.parseDateTime(runTimeString);
+				if(runTime!=null)
+					break;
+			}
+		}
+		return runTimeString;
 	}
 }
