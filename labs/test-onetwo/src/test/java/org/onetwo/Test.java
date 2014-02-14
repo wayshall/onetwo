2f2f4210b36505d118c9d4ac8d6ae9904ad32de0
wayshall <weishao.zeng@gmail.com>
2014-02-14 12:39:08 +0800

    add merge file method

--- labs/test-onetwo/src/test/java/org/onetwo/Test.java
+++ labs/test-onetwo/src/test/java/org/onetwo/Test.java
@@ -1,13 +1,16 @@
 package org.onetwo;
 
+import java.io.File;
+
+import org.onetwo.common.utils.FileUtils;
+
 
 
 
 public class Test {
 	
 	public static void main(String[] args){
-		int i = '\0';
-		System.out.println("i: " + i);
+		FileUtils.mergeFiles("unicode", "D:/mydev/workspace/dbproc/proc/newfile.sql", "D:/mydev/workspace/dbproc/proc/", ".sql");
 	}
 	
 }
