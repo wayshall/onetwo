a61a5f63e92641133550dd3070877d9719d964a3
wayshall <weishao.zeng@gmail.com>
2014-02-25 11:00:27 +0800

    add commonbizutils

--- labs/test-onetwo/src/test/java/org/onetwo/common/utils/CommonBizUtilsTest.java
+++ labs/test-onetwo/src/test/java/org/onetwo/common/utils/CommonBizUtilsTest.java
@@ -7,8 +7,17 @@ public class CommonBizUtilsTest {
 	
 	@Test
 	public void testGeneratedIdCardNo(){
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 			String cardNo = CommonBizUtils.generatedIdCardNo();
+			boolean rs = CommonBizUtils.checkIdCardNo(cardNo);
+			if(!rs){
+				System.out.println("cardno["+cardNo+"] error");
+			}
+			Assert.assertTrue(rs);
+		}
+		for (int i = 0; i < 100; i++) {
+			String cardNo = CommonBizUtils.generatedIdCardNo("广东", "1984");
+			System.out.println("cardno["+cardNo+"]");
 			boolean rs = CommonBizUtils.checkIdCardNo(cardNo);
 			if(!rs){
 				System.out.println("cardno["+cardNo+"] error");
