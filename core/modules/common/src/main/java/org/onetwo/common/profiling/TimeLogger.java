376e8fb3c58063bab6c70528b069388aeff6f883
wayshall <weishao.zeng@gmail.com>
2014-03-16 01:43:33 +0800

    modify timelogger

--- core/modules/common/src/main/java/org/onetwo/common/profiling/TimeLogger.java
+++ core/modules/common/src/main/java/org/onetwo/common/profiling/TimeLogger.java
@@ -2,7 +2,7 @@ package org.onetwo.common.profiling;
 
 public interface TimeLogger {
 
-	TimeLogger INSTANCE = new Log4jTimeLogger();
+	TimeLogger INSTANCE = new Log4jTimeLogger(TimeLogger.class);
 	void log(String msg);
 
 }
