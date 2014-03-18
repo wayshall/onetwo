3f1bc2127c1ee7ff2930a6fd8fa154b001214f70
wayshall <weishao.zeng@gmail.com>
2014-03-18 13:57:14 +0800

    task-sys

--- labs/task-system/src/main/java/org/onetwo/app/task/MainTaskProcessor.java
+++ labs/task-system/src/main/java/org/onetwo/app/task/MainTaskProcessor.java
@@ -1,11 +1,34 @@
 package org.onetwo.app.task;
 
+import java.util.List;
+
 
 public class MainTaskProcessor {
 	
 	private SimpleTaskQueue taskQueue;
+	private SimpleTaskListenerRegistry taskListenerRegistry;
 	
-	public void doTask(){
+	public MainTaskProcessor(SimpleTaskQueue taskQueue) {
+		super();
+		this.taskQueue = taskQueue;
+	}
+
+	public void doTask() throws InterruptedException{
+		TaskData task = null;
+		for(;;){
+			task = taskQueue.take();
+			if(task==null)
+				continue;
+			doProcess(task);
+		}
+	}
+	
+	protected void doProcess(TaskData task){
+		List<TaskListener> listeners = this.taskListenerRegistry.getTaskListenerGroup(task.getType()).getListeners();
+		for(TaskListener taskListener : listeners){
+			Object result = taskListener.onExecute(task);
+			taskListener.afterExecute(task, result);
+		}
 	}
 
 }
