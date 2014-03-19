ecfdf17bba1af215ddb067c4998efee0a45a015b
wayshall <weishao.zeng@gmail.com>
2014-03-19 17:29:44 +0800

    excel export again!

--- core/modules/poi/src/main/java/org/onetwo/common/excel/RowDataContext.java
+++ core/modules/poi/src/main/java/org/onetwo/common/excel/RowDataContext.java
@@ -1,13 +1,19 @@
 package org.onetwo.common.excel;
 
+import java.util.Map;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.onetwo.common.excel.DefaultRowProcessor.CellContext;
+
+import com.google.common.collect.Maps;
 
 public class RowDataContext {
 	final private RowModel rowModel;
 	final private SheetData sheetData;
 	final private WorkbookData workbookData;
 	private Row currentRow;
+	private Map<String, CellContext> cellConextMap = Maps.newHashMap();
 	
 	public RowDataContext(WorkbookData workbookData, SheetData sheetData, RowModel rowModel) {
 		super();
@@ -32,5 +38,13 @@ public class RowDataContext {
 	}
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
+	}
+	
+	public void putCellContext(String name, CellContext cellContext){
+		this.cellConextMap.put(name, cellContext);
+	}
+	
+	public CellContext getCellContext(String name){
+		return this.cellConextMap.get(name);
 	}
 }
