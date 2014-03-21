e8b26d0d2a71dcc0820a1982b3cf917b228c431f
wayshall <weishao.zeng@gmail.com>
2014-03-21 18:13:39 +0800

    remove some bad code

--- core/modules/common/src/main/java/org/onetwo/common/utils/list/TriggerFun.java
+++
@@ -1,8 +0,0 @@
-package org.onetwo.common.utils.list;
-
-@Deprecated
-public interface TriggerFun<T> extends ListFun<T>{
-	
-	public boolean isTriggered(T element, int index, EasyList<T> easytor, Object...objects);
-	
-}
