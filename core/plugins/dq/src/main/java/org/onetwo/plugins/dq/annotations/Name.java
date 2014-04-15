e89ec3e9f5070aeac33273a196eba059241edaaa
wayshall <weishao.zeng@gmail.com>
2014-04-15 11:16:24 +0800

    add ifnull to name annotaiton

--- core/plugins/dq/src/main/java/org/onetwo/plugins/dq/annotations/Name.java
+++ core/plugins/dq/src/main/java/org/onetwo/plugins/dq/annotations/Name.java
@@ -15,6 +15,10 @@ public @interface Name {
 	
 	public boolean queryParam() default true;
 	
+	/****
+	 * 注意，此处设置只对query.setParameter有用，生成的sql仍需自己控制是否生成参数
+	 * @return
+	 */
 	public IfNull ifParamNull() default IfNull.Calm;
 	
 	/***
