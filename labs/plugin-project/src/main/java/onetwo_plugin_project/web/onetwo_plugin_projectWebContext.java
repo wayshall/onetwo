08697f0a52ce04e0329559bdf2d9f2a37bedc010
wayshall <weishao.zeng@gmail.com>
2014-04-29 11:28:56 +0800

    sso

---
+++ labs/plugin-project/src/main/java/onetwo_plugin_project/web/onetwo_plugin_projectWebContext.java
@@ -0,0 +1,12 @@
+package onetwo_plugin_project.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackageClasses=onetwo_plugin_projectWebContext.class)
+public class onetwo_plugin_projectWebContext {
+	
+	
+}
