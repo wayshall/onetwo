ae3205a8ea09d5301742a47dbf25858ae30f07e2
wayshall <weishao.zeng@gmail.com>
2014-04-29 11:28:56 +0800

    sso

---
+++ core/plugins/permission/src/main/java/org/onetwo/plugins/permission/utils/MenuMetaFields.java
@@ -0,0 +1,10 @@
+package org.onetwo.plugins.permission.utils;
+
+public class MenuMetaFields {
+	public static final String SYSCODE = "syscode";
+	public static final String SORT = "sort";
+	public static final String PERMISSION_TYPE = "permissionType";
+	public static final String HIDDEN = "hidden";
+	public static final String NAME = "name";
+	private MenuMetaFields(){}
+}
