e0ef474eb285f3e52f95543873acba47aa129289
wayshall <weishao.zeng@gmail.com>
2014-05-23 10:07:06 +0800

    test

--- core/plugins/batch/src/main/java/org/onetwo/plugins/batch/utils/BatchUtils.java
+++ core/plugins/batch/src/main/java/org/onetwo/plugins/batch/utils/BatchUtils.java
@@ -1,5 +1,6 @@
 package org.onetwo.plugins.batch.utils;
 
+import org.onetwo.common.utils.LangUtils;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -16,12 +17,17 @@ public class BatchUtils {
 		return newFlatFileItemReader(new FileSystemResource(path), 1, DelimitedLineTokenizer.DELIMITER_COMMA, title, mapper);
 	}
 
+	public static <T> SimpleFlatFileItemReader<T> newFlatFileItemReader(String path, FieldSetMapper<T> mapper){
+		return newFlatFileItemReader(new FileSystemResource(path), 0, DelimitedLineTokenizer.DELIMITER_COMMA, null, mapper);
+	}
+
 	public static <T> SimpleFlatFileItemReader<T> newFlatFileItemReader(String path, int linesToSkip, String delimiter, String title, FieldSetMapper<T> mapper){
 		return newFlatFileItemReader(new FileSystemResource(path), linesToSkip, delimiter, title, mapper);
 	}
 	public static <T> SimpleFlatFileItemReader<T> newFlatFileItemReader(Resource resource, int linesToSkip, String delimiter, String title, FieldSetMapper<T> mapper){
 		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(delimiter);
-		tokenizer.setNames(StringUtils.tokenizeToStringArray(title, delimiter));
+		if(org.onetwo.common.utils.StringUtils.isNotBlank(title))
+			tokenizer.setNames(StringUtils.tokenizeToStringArray(title, delimiter));
 		DefaultLineMapper<T> lineMapper = new DefaultLineMapper<T>();
 		lineMapper.setLineTokenizer(tokenizer);
 		lineMapper.setFieldSetMapper(mapper);
@@ -30,6 +36,7 @@ public class BatchUtils {
 		reader.setLineMapper(lineMapper);
 		reader.setResource(resource);
 		reader.setLinesToSkip(linesToSkip);
+		reader.setEncoding(LangUtils.UTF8);
 		/*try {
 			reader.afterPropertiesSet();
 		} catch (Exception e) {
