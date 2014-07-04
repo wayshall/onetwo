e570b3f65d9a2916ed501ef4453741c8f76511b3
wayshall <weishao.zeng@gmail.com>
2014-07-04 12:12:39 +0800

    dq query and table.js

--- labs/test-onetwo/src/test/java/org/onetwo/common/spring/sql/NamedSqlTest.java
+++ labs/test-onetwo/src/test/java/org/onetwo/common/spring/sql/NamedSqlTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.onetwo.common.spring.sql.SqlUtils.ParsedSqlWrapper;
+import org.onetwo.common.spring.sql.SqlUtils.ParsedSqlWrapper.SqlParamterMeta;
 import org.onetwo.common.utils.LangUtils;
 import org.onetwo.common.utils.ReflectUtils;
 import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
@@ -27,6 +29,46 @@ public class NamedSqlTest {
 		List<int[]> parameterIndexs = (List<int[]>) ReflectUtils.getFieldValue(psql, "parameterIndexes");
 		for(int[] index : parameterIndexs){
 			System.out.println(LangUtils.toString(index));
+		}
+	}
+	
+	@Test
+	public void testSimpleNamedSql2(){
+		sql = "select * from admin_user t where t.userName=:user.userName?likString and (t.age=:user.age or t.age=:2) and t.idcode=?";
+		ParsedSql psql = NamedParameterUtils.parseSqlStatement(sql);
+		List<String> parameterNames = (List<String>) ReflectUtils.getFieldValue(psql, "parameterNames");
+		for(String pname : parameterNames){
+			System.out.println(pname);
+		}
+		Assert.assertEquals("user.userName?likString", parameterNames.get(0));
+		Assert.assertEquals("user.age", parameterNames.get(1));
+		List<int[]> parameterIndexs = (List<int[]>) ReflectUtils.getFieldValue(psql, "parameterIndexes");
+		for(int[] index : parameterIndexs){
+			System.out.println(LangUtils.toString(index));
+		}
+	}
+	
+	@Test
+	public void testSqlUtils(){
+		sql = "select * from admin_user t where (t.userName=:user.userName?like or t.userName=:user.userName?prelike or t.userName=:user.userName?postlike) and (t.age=:user.age or t.age=:2) and t.idcode=?";
+		ParsedSqlWrapper sqlWrapper = SqlUtils.parseSql(sql);
+		for(SqlParamterMeta parameter : sqlWrapper.getParameters()){
+			System.out.println("name: " + parameter);
+			if("like".equals(parameter.getFunction())){
+				Object val = ReflectUtils.invokeMethod(parameter.getFunction(), SqlParamterFunctions.getInstance(), "way");
+				System.out.println("val:"+ val);
+				Assert.assertEquals("%way%", val);
+			}
+			if("prelike".equals(parameter.getFunction())){
+				Object val = ReflectUtils.invokeMethod(parameter.getFunction(), SqlParamterFunctions.getInstance(), "way");
+				System.out.println("val:"+ val);
+				Assert.assertEquals("%way", val);
+			}
+			if("postlike".equals(parameter.getFunction())){
+				Object val = ReflectUtils.invokeMethod(parameter.getFunction(), SqlParamterFunctions.getInstance(), "way");
+				System.out.println("val:"+ val);
+				Assert.assertEquals("way%", val);
+			}
 		}
 	}
 
