c6e71e68c6a3416f7b9266efad14d5045c6c413e
wayshall <weishao.zeng@gmail.com>
2014-07-20 23:47:28 +0800

    table generator

---
+++ core/plugins/hibernate/src/main/java/org/onetwo/common/hibernate/TableGeneratorService.java
@@ -0,0 +1,11 @@
+package org.onetwo.common.hibernate;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface TableGeneratorService {
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public long generatedValue(String seqName);
+
+}
