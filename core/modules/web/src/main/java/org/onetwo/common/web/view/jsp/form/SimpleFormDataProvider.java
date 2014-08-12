2497fefb5d2fbbad7a270f515d8ce82afe91e5ef
wayshall <weishao.zeng@gmail.com>
2014-08-12 17:39:51 +0800

    fix form field tag

--- core/modules/web/src/main/java/org/onetwo/common/web/view/jsp/form/SimpleFormDataProvider.java
+++ core/modules/web/src/main/java/org/onetwo/common/web/view/jsp/form/SimpleFormDataProvider.java
@@ -2,6 +2,8 @@ package org.onetwo.common.web.view.jsp.form;
 
 import org.onetwo.common.log.MyLoggerFactory;
 import org.onetwo.common.spring.SpringUtils;
+import org.onetwo.common.utils.LangUtils;
+import org.onetwo.common.utils.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeansException;
@@ -30,16 +32,24 @@ public class SimpleFormDataProvider implements FormDataProvider {
 
 	@Override
 	public Object getFieldValue(FormFieldTagBean field) {
+		String fieldValue = field.getValue();
+		if(StringUtils.isBlank(fieldValue))
+			return LangUtils.EMPTY_STRING;
+
 		Object val = "";
 		if(field.isModelAttribute()){
 			if(this.accessor!=null){
 				try {
-					if(this.accessor.isReadableProperty(field.getValue()))
-						val = this.accessor.getPropertyValue(field.getValue());
+					if(this.accessor.isReadableProperty(fieldValue))
+						val = this.accessor.getPropertyValue(fieldValue);
+					else
+						val = fieldValue;
 				} catch (BeansException e) {
 					logger.error("getPropertyValue error : " + e.getMessage());
 				}
 			}
+		}else{
+			val = fieldValue;
 		}
 		return val;
 	}
