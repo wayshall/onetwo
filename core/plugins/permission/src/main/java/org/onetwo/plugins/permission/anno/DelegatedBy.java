3bec5ab37b822134d58ef4a966cf8ff9f5ee9f14
wayshall <weishao.zeng@gmail.com>
2014-08-25 22:52:02 +0800

    menu new parser

--- core/plugins/permission/src/main/java/org/onetwo/plugins/permission/anno/DelegatedBy.java
+++ core/plugins/permission/src/main/java/org/onetwo/plugins/permission/anno/DelegatedBy.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/****
+ * 用在被代理的菜单节点上
+ * @author wayshall
+ *
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DelegatedBy {
