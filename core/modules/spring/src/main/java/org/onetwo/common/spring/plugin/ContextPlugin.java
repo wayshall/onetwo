1990b0985de4bb482fc6a59a30ca1983898758f5
wayshall <weishao.zeng@gmail.com>
2014-08-26 23:30:45 +0800

    add crud to controller

--- core/modules/spring/src/main/java/org/onetwo/common/spring/plugin/ContextPlugin.java
+++ core/modules/spring/src/main/java/org/onetwo/common/spring/plugin/ContextPlugin.java
@@ -2,6 +2,12 @@ package org.onetwo.common.spring.plugin;
 
 import java.util.List;
 
+/****
+ * 插件类一般放在插件项目的根目录，以识别是否属于插件类
+ * {@link org.onetwo.common.fish.plugin.JFishPluginMeta#isClassOfThisPlugin JFishPluginMeta#isClassOfThisPlugin} 
+ * @author wayshall
+ *
+ */
 public interface ContextPlugin {
 
 	void init(ContextPluginMeta pluginMeta, String appEnv);
