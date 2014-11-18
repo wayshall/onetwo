751277492a1d009467ce22f6d642f05a5ec78d33
wayshall <weishao.zeng@gmail.com>
2014-11-18 15:19:08 +0800

    add autoSizeColumn to excel generator

--- core/modules/poi/src/main/java/org/onetwo/common/excel/POIExcelGeneratorImpl.java
+++ core/modules/poi/src/main/java/org/onetwo/common/excel/POIExcelGeneratorImpl.java
@@ -198,15 +198,37 @@ public class POIExcelGeneratorImpl extends AbstractWorkbookExcelGenerator implem
 		sdata.setSheet(sheet);
 		this.buildColumnWidth(sdata);
 		this.generateSheet(sdata);
+		this.buildAutoColumnSize(sdata);
 	}
 	
 	private void buildColumnWidth(SheetData sdata){
 		String columnWidth = sdata.getSheetModel().getColumnWidth();
-		columnWidth = (String)sdata.parseValue(columnWidth);
+		if(StringUtils.isNotBlank(columnWidth) && columnWidth.startsWith("#")){
+			columnWidth = (String)sdata.parseValue(columnWidth);
+		}
 		Map<Integer, Short> columnMap = this.propertyStringParser.parseColumnwidth(columnWidth);
 		if(LangUtils.isNotEmpty(columnMap)){
 			for(Entry<Integer, Short> entry : columnMap.entrySet()){
 				sdata.getSheet().setColumnWidth(entry.getKey(), entry.getValue()*256);
+			}
+		}
+	}
+
+	
+	private void buildAutoColumnSize(SheetData sdata){
+		Sheet sheet = sdata.getSheet();
+		if(sheet.getPhysicalNumberOfRows()>0 && sdata.getSheetModel().isAutoSizeColumn()){
+			int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+			boolean useMerged = sdata.getSheetModel().isUseMergedCells();
+			for (int i = 0; i < cellCount; i++) {
+				sheet.autoSizeColumn(i, useMerged);
+			}
+		}else{
+			Map<Short, Boolean> columSize = sdata.getSheetModel().getAutoSizeColumnMap();
+			if(LangUtils.isEmpty(columSize))
+				return ;
+			for(Entry<Short, Boolean> entry : columSize.entrySet()){
+				sheet.autoSizeColumn(entry.getKey(), entry.getValue());
 			}
 		}
 	}
