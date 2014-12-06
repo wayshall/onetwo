c0d53d7c3dda6f477215a284363b4a0ebdf11f2b
wayshall <weishao.zeng@gmail.com>
2014-12-07 04:16:38 +0800

    modify apptimer

--- core/projects/apptimer/src/main/resources/run.bat
+++ core/projects/apptimer/src/main/resources/run.bat
@@ -1,3 +1,7 @@
 @echo off
-java -Xms1024m -Xmx1024m -jar apptimer-${project.version}.jar
+set JAVA_HOME=
+set PATH=%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin
+set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar
+set JAVA_OPTS="-server -Xms2048m -Xmx2048m -XX:MaxPermSize=512m"
+java -jar ${artifactId}-${project.version}.jar
 pause
