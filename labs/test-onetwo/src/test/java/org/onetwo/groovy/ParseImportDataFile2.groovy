f798843d256986cd04a9096d1156463da480b271
wayshall <weishao.zeng@gmail.com>
2014-12-20 00:50:57 +0800

    change groovy version

--- labs/test-onetwo/src/test/java/org/onetwo/groovy/ParseImportDataFile2.groovy
+++ labs/test-onetwo/src/test/java/org/onetwo/groovy/ParseImportDataFile2.groovy
@@ -3,44 +3,33 @@ package org.onetwo.groovy
 import org.onetwo.common.utils.FileUtils;
 
 
-String path = this.getClass().getResource("").getPath()
-println "path:$path"
-File dir = new File(path)
+String basePath = "D:/mydev/workspace/onetwo/labs/test-onetwo/src/test/java/org/onetwo/groovy"
+//this.getClass().getResource("").getPath()
+println "path:$basePath"
+File errorFile2 = new File("${basePath}/467-result-error-2.txt")
 
-def Map<String, String> datalist=[:]
-dir.eachFile { file ->
-	if(file.name.equals("467-result-error-2.txt")){
-		file.splitEachLine("\\s") {
-			def strs = it[1].split("\\[|\\]")
-//			println strs
-			/*def ci = new CardInfo()
-			ci.lcn=strs[0][0..15]
-			ci.idcard=strs[1]
-			datalist += ci*/
-			datalist[strs[0][0..15]]=strs[1]
-		}
-	}
+def datalist=[:]
+errorFile2.splitEachLine("\\s") {
+	def strs = it[1].split("\\[|\\]")
+	datalist[strs[0][0..15]]=strs[1]
 }
 
 println "datalist: $datalist"
 
+//用正确的身份证替换报盘文件里的姓名
+File resultFile1 = new File("${basePath}/467-result-1.txt")
 def index = 0
 def newDatalist = []
-dir.eachFile { file ->
-	if(file.getName().equals("467-result-1.txt")){
-		file.splitEachLine("\t") {
-//			println it
-			if(datalist.containsKey(it[3])){
-				it[0] = datalist[it[3]]
-				println "$index:$it"
-				index++
-				newDatalist.add(it)
-			}
-		}
+resultFile1.splitEachLine("\t") {
+	if(datalist.containsKey(it[3])){
+		it[0] = datalist[it[3]]
+		println "$index:$it"
+		index++
+		newDatalist.add(it)
 	}
 }
 
-def BufferedWriter newWirter = new File("D:/mydev/workspace/onetwo/labs/test-onetwo/src/test/java/org/onetwo/groovy/467-result-2.txt").newWriter("GBK")
+def BufferedWriter newWirter = new File("${basePath}/467-result-2.txt").newWriter("GBK")
 try {
 	newDatalist.each {
 		println it
