356d75b4111756f325ec1f63cbe1a9d49ad46b64
wayshall <weishao.zeng@gmail.com>
2015-01-23 00:57:50 +0800

    Dictionary add enum supported

--- labs/test-onetwo/src/test/java/org/onetwo/Test.java
+++ labs/test-onetwo/src/test/java/org/onetwo/Test.java
@@ -8,9 +8,8 @@ public class Test {
 	public static void main(String[] args) throws IOException{
 		String a = "abc";
 		String b = "a";
-		String c= b+"bc";
-		Object o = new Object();
-		System.out.println("o:" + o.toString());
+		StringBuilder c= new StringBuilder();
+		System.out.println("o:" + c.toString());
 	}
 	
 }
