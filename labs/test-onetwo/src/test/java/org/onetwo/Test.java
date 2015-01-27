53e89b0ebd65a7e652f95d6ee52b92cf2038ad7d
wayshall <weishao.zeng@gmail.com>
2015-01-28 01:42:48 +0800

    upgrade to spring 3.2.11

--- labs/test-onetwo/src/test/java/org/onetwo/Test.java
+++ labs/test-onetwo/src/test/java/org/onetwo/Test.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) throws IOException{
-		String a = "abc";
-		String b = "a";
-		StringBuilder c= new StringBuilder();
-		System.out.println("o:" + c.toString());
+		int a = 444444;
+		int b = Integer.MAX_VALUE;
+		int c = a + b;
+		
+		String s1 = "a";
+		String s2 = "b";
+		String s3 = s1 + s2;
 	}
 	
 }
