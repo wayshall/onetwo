966dca788c35e0fa69a9820c630cf02791539b6c
wayshall <weishao.zeng@gmail.com>
2015-02-02 22:46:02 +0800

    test

---
+++ labs/test-onetwo/src/test/java/org/onetwo/camel/TestRouter.java
@@ -0,0 +1,34 @@
+package org.onetwo.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.onetwo.common.utils.LangUtils;
+
+public class TestRouter extends RouteBuilder {
+	
+	public static class UrlBean {
+		public String getReportUrl(){
+			System.out.println("test");
+			return "outer/cardQuery/status.json?idcard=441827198403057750";
+		}
+	}
+
+	@Override
+	public void configure() throws Exception {
+//		String brokerURL = "tcp://localhost:61616";
+//		getContext().addComponent("activemq", ActiveMQComponent.activeMQComponent(brokerURL));
+//		getContext().addComponent("http", HttpComponent.);
+		
+//		from("activemq:queue:web.operation")
+		System.out.println("TestRouter~~~");
+//		from("direct:start")
+		from("timer:foo?period=5s")
+			.bean(UrlBean.class, "getReportUrl")
+			.to("file:D:/camel/test?fileName=test");
+			/*.recipientList(simple("http://localhost:8080/iccard-web/${body}"))
+			.to("file:D:/camel/test?fileName=test")*/;
+		LangUtils.println("TestRouter end~~~");
+	}
+	
+	
+
+}
