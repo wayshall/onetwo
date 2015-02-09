51d733fb5276bf9b46fd4fe8f2cc2a9704e31587
wayshall <weishao.zeng@gmail.com>
2015-02-09 22:55:48 +0800

    rafactor plugin events

--- core/modules/jfish/src/main/java/org/onetwo/common/fish/plugin/JFishMvcConfigurerListenerAdapter.java
+++ core/modules/jfish/src/main/java/org/onetwo/common/fish/plugin/JFishMvcConfigurerListenerAdapter.java
@@ -1,18 +1,20 @@
 package org.onetwo.common.fish.plugin;
 
-import org.onetwo.common.spring.web.mvc.config.JFishMvcConfigurerListener;
+import org.onetwo.common.spring.web.mvc.config.JFishMvcPluginListener;
 import org.onetwo.common.spring.web.mvc.config.event.ArgumentResolverEvent;
 import org.onetwo.common.spring.web.mvc.config.event.FreeMarkerConfigurerBuildEvent;
 import org.onetwo.common.spring.web.mvc.config.event.MvcContextConfigRegisterEvent;
 import org.onetwo.common.spring.web.mvc.config.event.MvcContextInitEvent;
 import org.onetwo.common.spring.web.mvc.config.event.PropertyEditorRegisterEvent;
+import org.onetwo.common.spring.web.mvc.config.event.WebApplicationStartupEvent;
+import org.onetwo.common.spring.web.mvc.config.event.WebApplicationStopEvent;
 
 /****
  * 改用eventbus实现后的适配
  * @author way
  *
  */
-public class JFishMvcConfigurerListenerAdapter implements JFishMvcConfigurerListener {
+public class JFishMvcConfigurerListenerAdapter implements JFishMvcPluginListener {
 
 	final private AbstractJFishPlugin<?> jfishPlugin;
 	
@@ -21,27 +23,31 @@ public class JFishMvcConfigurerListenerAdapter implements JFishMvcConfigurerList
 		this.jfishPlugin = jfishPlugin;
 	}
 
+	
+	@Override
+	public void listening(WebApplicationStartupEvent event) {
+	}
+
+
+	@Override
+	public void listening(WebApplicationStopEvent event) {
+	}
+
+
 	@Override
 	public void listening(FreeMarkerConfigurerBuildEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void listening(PropertyEditorRegisterEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void listening(MvcContextInitEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void listening(ArgumentResolverEvent event) {
-		
 	}
 
 	@Override
--- core/modules/jfish/src/main/java/org/onetwo/common/spring/web/mvc/config/JFishMvcPluginListener.java
+++ core/modules/jfish/src/main/java/org/onetwo/common/spring/web/mvc/config/JFishMvcPluginListener.java
@@ -5,10 +5,23 @@ import org.onetwo.common.spring.web.mvc.config.event.FreeMarkerConfigurerBuildEv
 import org.onetwo.common.spring.web.mvc.config.event.MvcContextConfigRegisterEvent;
 import org.onetwo.common.spring.web.mvc.config.event.MvcContextInitEvent;
 import org.onetwo.common.spring.web.mvc.config.event.PropertyEditorRegisterEvent;
+import org.onetwo.common.spring.web.mvc.config.event.WebApplicationStartupEvent;
+import org.onetwo.common.spring.web.mvc.config.event.WebApplicationStopEvent;
 
 import com.google.common.eventbus.Subscribe;
 
-public interface JFishMvcConfigurerListener {
+/****
+ * 监听器接口
+ * 显式定义监听器接口，以使代码逻辑更清晰
+ * @author way
+ *
+ */
+public interface JFishMvcPluginListener {
+
+	@Subscribe
+	void listening(WebApplicationStartupEvent event);
+	@Subscribe
+	void listening(WebApplicationStopEvent event);
 
 	@Subscribe
 	void listening(FreeMarkerConfigurerBuildEvent event);
@@ -20,8 +33,8 @@ public interface JFishMvcConfigurerListener {
 	void listening(MvcContextInitEvent event);
 	
 	@Subscribe
-	void listening(final ArgumentResolverEvent event);
+	void listening(ArgumentResolverEvent event);
 	
 	@Subscribe
-	void listening(final MvcContextConfigRegisterEvent event);
+	void listening(MvcContextConfigRegisterEvent event);
 }
