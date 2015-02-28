f3eee4a7be4ff2f300d479837b70cdaa99471e58
wayshall <weishao.zeng@gmail.com>
2015-02-28 18:07:44 +0800

    move session config to basesteiconfig

--- core/plugins/session/src/main/java/org/onetwo/plugins/session/web/SpringSessionInitializer.java
+++ core/plugins/session/src/main/java/org/onetwo/plugins/session/web/SpringSessionInitializer.java
@@ -3,26 +3,23 @@ package org.onetwo.plugins.session.web;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-import org.onetwo.common.spring.SpringUtils;
-import org.onetwo.common.utils.propconf.JFishProperties;
-import org.onetwo.plugins.session.SessionPlugin;
-import org.onetwo.plugins.session.SessionPluginConfig;
+import org.onetwo.common.web.config.BaseSiteConfig;
 
 public class SpringSessionInitializer extends AbstractHttpSessionApplicationInitializer {
 	
 //	private static boolean initialized = false;
-	private SessionPluginConfig sessionConfig;
+//	private SessionPluginConfig sessionConfig;
 	
 	public SpringSessionInitializer(){
 		super(SessionPluginWebContext.class);
-		JFishProperties properties = SpringUtils.loadAsJFishProperties(SessionPlugin.CONFIG_PATH);
+		/*JFishProperties properties = SpringUtils.loadAsJFishProperties(SessionPlugin.CONFIG_PATH);
 		sessionConfig = new SessionPluginConfig();
-		sessionConfig.load(properties);
+		sessionConfig.load(properties);*/
 	}
 
 
     public void onStartup(ServletContext servletContext) throws ServletException {
-    	if(!sessionConfig.isContainerSession()){
+    	if(!BaseSiteConfig.getInstance().isContainerSession()){
         	super.onStartup(servletContext);
     	}
     }
