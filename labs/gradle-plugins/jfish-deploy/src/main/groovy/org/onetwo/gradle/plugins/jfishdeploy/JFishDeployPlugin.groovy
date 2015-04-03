ad3a6b09425189964c72e1108f14742a97dab648
wayshall <weishao.zeng@gmail.com>
2015-04-03 23:32:32 +0800

    deploy plugin

--- labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/JFishDeployPlugin.groovy
+++ labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/JFishDeployPlugin.groovy
@@ -6,8 +6,12 @@ import org.gradle.api.Project
 class JFishDeployPlugin implements Plugin<Project> {
 
 	@Override
-	public void apply(Project project) {
+	def void apply(Project project) {
+		def configName = System.getProperty("profile")
+		def configFile = project.file(configName.endsWith('.groovy')?configName:"${configName}.groovy")
+		new ConfigSlurper(profile).parse(configFile.toURL())
+		
 		project.task("deployTomcat", type: DeployTomcatTask)
 	}
-
+	
 }
