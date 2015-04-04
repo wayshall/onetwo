42acc14ba2ae59808bbd246c97fd8be547bc8dbf
wayshall <weishao.zeng@gmail.com>
2015-04-04 17:54:54 +0800

    finish jfishdeploy plugin

--- labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/DeployTomcatTask.groovy
+++ labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/DeployTomcatTask.groovy
@@ -1,30 +1,42 @@
 package org.onetwo.gradle.plugins.jfishdeploy
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.Project
 
 class DeployTomcatTask extends DefaultTask {
+	String profile;
 	
 	@TaskAction
 	public void deployTomcat(){
-		logger.lifecycle "copy war to servsers ..."
+		profile = project.extensions.findByName("profile")
+		Project project = getProject();
+		//def config = JFishDeployPlugin.loadGroovyConfig(project.getRootProject(), profile, "config")
+		//logger.lifecycle "deployTomcat loaded ${profile} config::"+config
+
+		logger.lifecycle "start deploy ..."
+		def config = project.extensions.findByName("jfishDeployer").config
 		def mainDir = config.deploy.tomcats[0].baseDir + "/webapps/" + project.name
-		copy {
-			logger.lifecycle "${mainDir} bak to ${config.deploy.bakDir}"
+		project.copy {
+			logger.lifecycle "${mainDir} bakup to ${config.deploy.bakDir}"
 			from mainDir
 			into "${config.deploy.bakDir}/${project.name}-${new Date().format('yyyyMMddHHmmss')}"
 		}
 		
 		config.deploy.tomcats.eachWithIndex { tc, index ->
 			def webappDir = "${tc.baseDir}/webapps"
-			copy {
+			project.copy {
 				logger.lifecycle "${project.name} deploy to ${webappDir}"
-				delete "${webappDir}/${project.name}"
-				with renameWarFile
+				project.delete "${webappDir}/${project.name}"
+				from project.war
+				rename { it.replace "-${project.version}", ''}
 				into webappDir
 			}
+			logger.lifecycle "execute ${tc.baseDir}/bin/${tc.starter} "
 			"cmd /c start ${tc.baseDir}/bin/${tc.starter}".execute()
 		}
+		
 	}
+
 
 }
--- labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/JFishDeployPlugin.groovy
+++ labs/gradle-plugins/jfish-deploy/src/main/groovy/org/onetwo/gradle/plugins/jfishdeploy/JFishDeployPlugin.groovy
@@ -2,16 +2,67 @@ package org.onetwo.gradle.plugins.jfishdeploy
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
+import org.gradle.api.tasks.Copy
+import groovy.util.ConfigObject
 
 class JFishDeployPlugin implements Plugin<Project> {
 
+	//private Logger logger =  Logging.getLogger(JFishDeployPlugin.class)
+
 	@Override
 	def void apply(Project project) {
-		def configName = System.getProperty("profile")
-		def configFile = project.file(configName.endsWith('.groovy')?configName:"${configName}.groovy")
-		new ConfigSlurper(profile).parse(configFile.toURL())
 		
-		project.task("deployTomcat", type: DeployTomcatTask)
+		def profile = System.getProperty("profile")
+		if(profile==null){
+			logger.lifecycle "no profile found!"
+			reutrn ;
+		}
+
+		project.extensions.add("profile", profile)
+
+		def config = loadGroovyConfig(project.getRootProject(), profile, "config")
+		JFishDeployer jfishDeployer = new JFishDeployer()
+		jfishDeployer.config = config
+		project.extensions.add("jfishDeployer", jfishDeployer)
+		//project.extraProperties.set("jdeployConfig", config)
+
+		project.task("deployTomcat", type: DeployTomcatTask).dependsOn("war")
+		def copyEnvConfig = project.task("copyEnvConfig", type: Copy){
+			//def config = JFishDeployPlugin.loadGroovyConfig(project.getRootProject(), profile, "config")
+			logger.lifecycle "copyEnvConfig loaded ${profile} config::"+config
+			if(config!=null){
+				logger.lifecycle "copy ${profile} config..."
+				from config.deploy.configDir
+				into "${project.buildDir}/resources/main"
+			}
+		}
+
+		project.tasks.war.dependsOn copyEnvConfig
+	}
+
+	public static loadGroovyConfig(project, profile, configName) {
+		Logger logger =  Logging.getLogger(JFishDeployPlugin.class)//project.getLogger()
+		def configFile = project.file(configName.endsWith('.groovy')?configName:"${configName}.groovy")
+		logger.lifecycle "load config file : ${configFile}"
+		def configData = new ConfigSlurper(profile).parse(configFile.toURL())
+		logger.lifecycle "loaded ${profile} config::"+configData
+		return configData;
+	}
+
+	class JFishDeployer {
+		ConfigObject config;
+
+		def embeddedTomcat() {
+			def tomcatVersion = "7.0.59";
+			def jspVersion = "2.1"
+			
+			["org.apache.tomcat.embed:tomcat-embed-core:$tomcatVersion",
+			 "org.apache.tomcat.embed:tomcat-embed-logging-log4j:$tomcatVersion",
+			 "org.apache.tomcat.embed:tomcat-embed-jasper:$tomcatVersion",
+			 "javax.servlet.jsp:jsp-api:${jspVersion}"]
+		}
 	}
 	
 }
