f5a8c3de520a88c9eaa017100a4f067d21f946b4
wayshall <weishao.zeng@gmail.com>
2015-04-19 23:22:43 +0800

    修改插件配置接口

--- core/modules/jfish/src/main/java/org/onetwo/common/fish/plugin/PluginConfig.java
+++ core/modules/jfish/src/main/java/org/onetwo/common/fish/plugin/PluginConfig.java
@@ -1,5 +1,6 @@
 package org.onetwo.common.fish.plugin;
 
+
 public interface PluginConfig {
 
 	void init(JFishPluginMeta jfishPluginMeta);
@@ -7,5 +8,9 @@ public interface PluginConfig {
 	public String getTemplatePath(String path);
 	
 	public String getContextPath();
+	
+	public String getBasedURL(String path);
+
+	public String getContextBasedPath(String path);
 
 }
