aeff7298306a95848aa0e160a1a4c3969c32b8a9
wayshall <weishao.zeng@gmail.com>
2015-04-30 16:34:13 +0800

    修改管理插件

--- core/plugins/permission/src/main/java/org/onetwo/plugins/permission/PermissionConfig.java
+++ core/plugins/permission/src/main/java/org/onetwo/plugins/permission/PermissionConfig.java
@@ -1,5 +1,7 @@
 package org.onetwo.plugins.permission;
 
+import java.lang.reflect.Field;
+
 import org.onetwo.common.utils.ReflectUtils;
 
 
@@ -7,7 +9,13 @@ import org.onetwo.common.utils.ReflectUtils;
 public interface PermissionConfig {
 
 	default public String getAppCode(){
-		String appCode = (String)ReflectUtils.getStaticFieldValue(getRootMenuClass(), "appCode");
+		String appCode = "";
+		Field appCodeField = ReflectUtils.findField(getRootMenuClass(), "appCode");
+		if(appCodeField!=null){
+			appCode = (String)ReflectUtils.getFieldValue(appCodeField, getRootMenuClass(), false);
+		}else{
+			appCode = getRootMenuClass().getSimpleName();
+		}
 		return appCode;
 	}
 	
--- core/projects/qxltkd/src/main/java/qingxinkd/utils/QingxinMenu.java
+++
@@ -1,8 +0,0 @@
-package qingxinkd.utils;
-
-
-public interface QingxinMenu {
-	String name = "清新连通宽带管理系统";
-	String appCode = "qingxin";
-
-}
