d53c85bbd3d001db5ecbf0decb6cc8d8e273bde6
wayshall <weishao.zeng@gmail.com>
2015-05-14 18:19:17 +0800

    rest

--- labs/test-onetwo/src/test/java/org/onetwo/Test.java
+++ labs/test-onetwo/src/test/java/org/onetwo/Test.java
@@ -10,9 +10,7 @@ public class Test {
 
 
 	public static void main(String[] args){
-		for(int i=0; i<100; i++){
-			System.out.println(i+":"+RandUtils.randomString(6));
-		}
+		System.out.println("teste:"+(null==Object.class));
 	}
 	
 	public static UserEntity createUser(){
