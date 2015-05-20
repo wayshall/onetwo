eceeae1f218bf1c3bdc8c25ab93e6546abd6cfc4
wayshall <weishao.zeng@gmail.com>
2015-05-20 19:00:16 +0800

    modify rest client

--- core/plugins/rest/src/main/java/org/onetwo/plugins/rest/RestClientFacotry.java
+++ core/plugins/rest/src/main/java/org/onetwo/plugins/rest/RestClientFacotry.java
@@ -1,17 +1,45 @@
 package org.onetwo.plugins.rest;
 
-import org.onetwo.common.spring.rest.JFishRestTemplate;
+import java.lang.reflect.Method;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 public class RestClientFacotry {
+	final private String baseUrl;
+	final private RestTemplate restTemplate;
+	final private Cache<Method, RestMethodResolver> methodCaches = CacheBuilder.newBuilder().weakKeys().softValues().build();
 	
-	public static <T> T create(Class<T> clazz){
-		RestClientHandler handler = new RestClientHandler(clazz);
-		return handler.getProxyObject();
+	public RestClientFacotry(String baseUrl, RestTemplate restTemplate) {
+		super();
+		this.baseUrl = baseUrl;
+		this.restTemplate = restTemplate;
 	}
-	
-	public static <T> T create(JFishRestTemplate restTemplate, Class<T> clazz){
-		RestClientHandler handler = new RestClientHandler(restTemplate, clazz);
+
+	public <T> T create(Class<T> clazz){
+		RestClientHandler handler = new RestClientHandler(baseUrl, restTemplate, methodCaches, clazz);
 		return handler.getProxyObject();
 	}
 
+	
+	public static class Builder {
+		private String baseUrl;
+		private RestTemplate restTemplate;
+
+		public Builder baseUrl(String baseUrl) {
+			this.baseUrl = baseUrl;
+			return this;
+		}
+		
+		public Builder restTemplate(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+			return this;
+		}
+
+		public RestClientFacotry build(){
+			return new RestClientFacotry(baseUrl, restTemplate);
+		}
+	}
 }
