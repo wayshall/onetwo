6d3b079611476413bb34e2f20ed95300897f92ef
wayshall <weishao.zeng@gmail.com>
2015-05-26 17:19:32 +0800

    3.4.3 release

--- core/projects/appweb-admin/src/main/java/appweb/admin/web/controller/test/TestController.java
+++ core/projects/appweb-admin/src/main/java/appweb/admin/web/controller/test/TestController.java
@@ -2,17 +2,24 @@ package appweb.admin.web.controller.test;
 
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.onetwo.common.utils.LangUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import appweb.admin.model.service.impl.TestServiceImpl;
 import appweb.admin.web.controller.WebBaseController;
 
 @Controller
 @RequestMapping("test")
 public class TestController extends WebBaseController {
 
+	@Resource
+	private TestServiceImpl testServiceImpl;
+	
+	
 	@RequestMapping("sayTo")
 	@ResponseBody
 	public String sayTo(String userName){
@@ -24,4 +31,12 @@ public class TestController extends WebBaseController {
 	public Map<?, ?> work(String userName, String workCity){
 		return LangUtils.asMap("userName", userName, "workCity", workCity);
 	}
+
+//	@RequestMapping(value="error", produces="application/json")
+	@RequestMapping(value="error")
+	@ResponseBody
+	public String error(){
+		return this.testServiceImpl.invokeError();
+	}
+	
 }
