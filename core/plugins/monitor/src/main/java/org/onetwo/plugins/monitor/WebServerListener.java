4802cd47a7e4e06a063671c2abc15b12926f4a48
wayshall <weishao.zeng@gmail.com>
2015-05-26 18:21:56 +0800

    修复 WebServerListener 编译错误

--- core/plugins/monitor/src/main/java/org/onetwo/plugins/monitor/WebServerListener.java
+++ core/plugins/monitor/src/main/java/org/onetwo/plugins/monitor/WebServerListener.java
@@ -1,14 +1,14 @@
 package org.onetwo.plugins.monitor;
 
-import org.onetwo.common.web.server.events.AfterWebappAddEvent;
-import org.onetwo.common.web.server.tomcat.EmbeddedTomcatListener;
+import org.onetwo.common.web.server.event.AfterWebappAddEvent;
+import org.onetwo.common.web.server.listener.EmbeddedServerListener;
 
 import com.google.common.eventbus.Subscribe;
 
-public class WebServerListener implements EmbeddedTomcatListener {
+public class WebServerListener implements EmbeddedServerListener {
 	
 	@Subscribe
-	public void lister(AfterWebappAddEvent event ){
+	public void lister(AfterWebappAddEvent<?> event ){
 		//http://hawt.io/configuration/index.html
 		System.setProperty("hawtio.authenticationEnabled", "false");
 	}
