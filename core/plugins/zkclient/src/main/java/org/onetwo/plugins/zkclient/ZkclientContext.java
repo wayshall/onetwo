3f1477773abe58b214864536349ec85c71fd48cb
wayshall <weishao.zeng@gmail.com>
2015-06-09 18:30:13 +0800

    把zkclien从webcontext移到appcontext

--- core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientContext.java
+++ core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientContext.java
@@ -1,5 +1,6 @@
 package org.onetwo.plugins.zkclient;
 
+import org.onetwo.plugins.zkclient.provider.BaseNodeRegister;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,23 @@ public class ZkclientContext {
 	public ZkclientPluginConfig zkclientPluginConfig(){
 		return ZkclientPlugin.getInstance().getConfig();
 	}
+
+	@Bean
+	public Zkclienter zkclienter(){
+		Zkclienter zk = new Zkclienter();
+		zk.setZkclientPluginConfig(zkclientPluginConfig());
+		return zk;
+	}
 	
+	@Bean
+	public ZkEventListenerRegister zkEventListenerRegister(){
+		return new ZkEventListenerRegister(zkclienter());
+	}
+	
+	@Bean
+	public BaseNodeRegister baseNodeRegister(){
+		BaseNodeRegister module = new BaseNodeRegister();
+		module.setZkclientPluginConfig(zkclientPluginConfig());
+		return module;
+	}
 }
--- core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientWebContext.java
+++ core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientWebContext.java
@@ -11,22 +11,4 @@ public class ZkclientWebContext {
 //	@Resource
 	private ZkclientPluginConfig zkclientPluginConfig = ZkclientPlugin.getInstance().getConfig();
 	
-	@Bean
-	public Zkclienter zkclienter(){
-		Zkclienter zk = new Zkclienter();
-		zk.setZkclientPluginConfig(zkclientPluginConfig);
-		return zk;
-	}
-	
-	@Bean
-	public ZkEventListenerRegister zkEventListenerRegister(){
-		return new ZkEventListenerRegister(zkclienter());
-	}
-	
-	@Bean
-	public BaseNodeRegister moduleRegister(){
-		BaseNodeRegister module = new BaseNodeRegister();
-		module.setZkclientPluginConfig(zkclientPluginConfig);
-		return module;
-	}
 }
