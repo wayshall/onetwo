0c341d4bc021e317e0a9c000455f37318836c326
wayshall <weishao.zeng@gmail.com>
2015-06-09 16:54:24 +0800

    #4 修改zkclient

--- core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientEvent.java
+++ core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientEvent.java
@@ -1,23 +1,21 @@
 package org.onetwo.plugins.zkclient;
 
 import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.ZooKeeper;
-import org.onetwo.plugins.zkclient.utils.ZkWrapper;
 
 public class ZkclientEvent {
 
 	final private WatchedEvent watchedEvent;
-	final private ZkWrapper zkWrapper;
-	public ZkclientEvent(WatchedEvent event, ZooKeeper zooKeeper) {
+	final private Zkclienter source;
+	public ZkclientEvent(WatchedEvent event, Zkclienter zooKeeper) {
 		super();
 		this.watchedEvent = event;
-		this.zkWrapper = ZkWrapper.wrap(zooKeeper);
+		this.source = zooKeeper;
 	}
 	public WatchedEvent getWatchedEvent() {
 		return watchedEvent;
 	}
-	public ZkWrapper getZkWrapper() {
-		return zkWrapper;
+	public Zkclienter getSource() {
+		return source;
 	}
 	
 }
