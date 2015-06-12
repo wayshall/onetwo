fd77c5fc749abcff913bab3b0aee252ccf53f609
wayshall <weishao.zeng@gmail.com>
2015-06-12 19:10:39 +0800

    update #4 为zk增加序列化接口

--- core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientContext.java
+++ core/plugins/zkclient/src/main/java/org/onetwo/plugins/zkclient/ZkclientContext.java
@@ -1,6 +1,7 @@
 package org.onetwo.plugins.zkclient;
 
 import org.onetwo.plugins.zkclient.curator.CuratorClient;
+import org.onetwo.plugins.zkclient.curator.JsonDataSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ public class ZkclientContext {
 	@Bean
 	public CuratorClient curatorClient(){
 		CuratorClient curator = new CuratorClient(zkclientPluginConfig());
+		curator.setDataSerializer(new JsonDataSerializer());
 		return curator;
 	}
 
