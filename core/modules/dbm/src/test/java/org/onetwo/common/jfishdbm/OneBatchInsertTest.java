33e94ae5f19d2ad31d3f55f3fa6f680bef50de13
wayshall <weishao.zeng@gmail.com>
2015-08-07 14:23:20 +0800

    修改mappedentryManager 修噶dbm test

--- core/modules/dbm/src/test/java/org/onetwo/common/jfishdbm/OneBatchInsertTest.java
+++ core/modules/dbm/src/test/java/org/onetwo/common/jfishdbm/OneBatchInsertTest.java
@@ -1,11 +1,18 @@
 package org.onetwo.common.jfishdbm;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+
 import javax.annotation.Resource;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.onetwo.common.jfishdbm.model.service.UserAutoidServiceImpl;
 import org.onetwo.common.profiling.TimeCounter;
+import org.onetwo.common.utils.LangUtils;
 import org.onetwo.common.utils.NiceDate;
 
 //@TransactionConfiguration(defaultRollback=true)
@@ -14,7 +21,7 @@ public class OneBatchInsertTest extends AppBaseTest {
 	@Resource
 	private UserAutoidServiceImpl userAutoidServiceImpl;
 	
-	@Test
+//	@Test
 	public void testBatchInsert(){
 		int insertCount = 10000;
 		//精确到秒，否则会有误差，比如2015-05-06 13:49:09.783存储到mysql后会变成2015-05-06 13:49:10，mysql的datetime只能精确到秒
@@ -26,6 +33,37 @@ public class OneBatchInsertTest extends AppBaseTest {
 		t.stop();
 		
 		userAutoidServiceImpl.deleteAll();
+	}
+	
+	@Test
+	public void testMultipThread() throws Exception{
+		int taskCount = 2;
+		CyclicBarrier barrier = new CyclicBarrier(taskCount);
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(taskCount);
+		Future<?> res1 = executor.submit(()->{
+			try {
+				System.out.println("await.....");
+				barrier.await();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			testBatchInsert();
+		});
+		LangUtils.await(2);
+		Future<?> res2 = executor.submit(()->{
+			try {
+				System.out.println("await.....");
+				barrier.await();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			testBatchInsert();
+		});
+
+		res1.get();
+		res2.get();
+		System.out.println("done");
+		
 	}
 
 }
