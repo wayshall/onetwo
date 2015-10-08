90ca2d6495a43a0289be0705dbdd0925bdf5579e
wayshall <weishao.zeng@gmail.com>
2015-10-08 15:13:01 +0800

    修改重构 FileNamedQueryManager 接口， 把接口的参数重构为接口 NamedQueryInvokeContext

--- core/modules/dbm/src/main/java/org/onetwo/common/db/filequery/AbstractFileNamedQueryFactory.java
+++ core/modules/dbm/src/main/java/org/onetwo/common/db/filequery/AbstractFileNamedQueryFactory.java
@@ -1,10 +1,9 @@
 package org.onetwo.common.db.filequery;
 
 import java.util.List;
-import java.util.Map;
 
 import org.onetwo.common.db.DataQuery;
-import org.onetwo.common.db.dquery.InvokeContext;
+import org.onetwo.common.db.dquery.NamedQueryInvokeContext;
 import org.onetwo.common.spring.ftl.TemplateParser;
 import org.onetwo.common.utils.LangUtils;
 
@@ -38,8 +37,8 @@ abstract public class AbstractFileNamedQueryFactory implements FileNamedQueryMan
 	}
 
 	@Override
-	public <E> E findOne(JFishNamedFileQueryInfo nameInfo, Object... params) {
-		DataQuery jq = this.createQuery(nameInfo, params);
+	public <E> E findOne(NamedQueryInvokeContext invokeContex) {
+		DataQuery jq = this.createQuery(invokeContex);
 		E entity = null;
 		List<E> list = jq.getResultList();
 		if(LangUtils.hasElement(list))
@@ -47,22 +46,22 @@ abstract public class AbstractFileNamedQueryFactory implements FileNamedQueryMan
 		return entity;
 	}
 	
-	@Override
+	/*@Override
 	public JFishNamedFileQueryInfo getNamedQueryInfo(String queryName){
 		JFishNamedFileQueryInfo queryInfo = sqlFileManager.getNamedQueryInfo(queryName);
 		return queryInfo;
-	}
+	}*/
 
 	@Override
-	public JFishNamedFileQueryInfo getNamedQueryInfo(InvokeContext invokeContex) {
+	public JFishNamedFileQueryInfo getNamedQueryInfo(NamedQueryInvokeContext invokeContex) {
 		String qname = invokeContex.getQueryName();
 		JFishNamedFileQueryInfo queryInfo = sqlFileManager.getNamedQueryInfo(qname);
 		return queryInfo;
 	}
 	@Override
-	public FileNamedSqlGenerator createFileNamedSqlGenerator(JFishNamedFileQueryInfo nameInfo, Map<Object, Object> params) {
-//		JFishNamedFileQueryInfo nameInfo = getNamedQueryInfo(queryName);
-		FileNamedSqlGenerator g = new DefaultFileNamedSqlGenerator(nameInfo, false, parser, params, queryProvideManager.getDataBase());
+	public FileNamedSqlGenerator createFileNamedSqlGenerator(NamedQueryInvokeContext invokeContext) {
+		JFishNamedFileQueryInfo nameInfo = getNamedQueryInfo(invokeContext);
+		FileNamedSqlGenerator g = new DefaultFileNamedSqlGenerator(nameInfo, false, parser, invokeContext.getParsedParams(), queryProvideManager.getDataBase());
 		return g;
 	}
 
