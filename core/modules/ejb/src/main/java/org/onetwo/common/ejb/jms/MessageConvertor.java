114a2f29dd188f07b17807470c7b95eb958b338a
wayshall <weishao.zeng@gmail.com>
2016-01-12 20:12:56 +0800

    remove unuse module

--- core/modules/ejb/src/main/java/org/onetwo/common/ejb/jms/MessageConvertor.java
+++
@@ -1,9 +0,0 @@
-package org.onetwo.common.ejb.jms;
-
-import javax.jms.Message;
-import javax.jms.Session;
-
-public interface MessageConvertor {
-
-	public Message convert(Session session, Object msg);
-}
