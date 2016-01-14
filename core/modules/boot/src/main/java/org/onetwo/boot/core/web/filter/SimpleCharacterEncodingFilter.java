75ca600a8c3d732cf7d56cf015cca9cbc5171aee
wayshall <weishao.zeng@gmail.com>
2016-01-14 15:45:37 +0800

    add simpple encodingfilter wrapper

---
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/filter/SimpleCharacterEncodingFilter.java
@@ -0,0 +1,25 @@
+package org.onetwo.boot.core.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.onetwo.common.log.JFishLoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.context.web.OrderedCharacterEncodingFilter;
+
+public class SimpleCharacterEncodingFilter extends OrderedCharacterEncodingFilter {
+	
+	private final Logger logger = JFishLoggerFactory.getLogger(this.getClass());
+
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		logger.info("=================>>>> set encoding: " + request.getRequestURL());
+		super.doFilterInternal(request, response, filterChain);;
+	}
+}
