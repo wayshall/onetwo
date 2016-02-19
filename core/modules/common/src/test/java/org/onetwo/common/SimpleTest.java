57b3e36fa361d2f925a00c8f6b4dccd421f3cdcc
wayshall <weishao.zeng@gmail.com>
2016-02-19 10:39:00 +0800

    add snowflake

--- core/modules/common/src/test/java/org/onetwo/common/SimpleTest.java
+++ core/modules/common/src/test/java/org/onetwo/common/SimpleTest.java
@@ -10,38 +10,9 @@ public class SimpleTest {
 	
 	@Test
 	public void test(){
-		String milis = "014";
-		BigInteger time = new BigInteger("13164449"+milis);
-		time = time.multiply(new BigInteger("20"));
-		time = time.add(new BigInteger(milis));
-		time = time.mod(new BigInteger("3000"));
-		System.out.println("time:" + time);
-
-		int alength = 100;
-		int[] a = new int[alength];
-		for(int i=0; i<alength; i++){
-			a[i] = i;
-		}
-
-		int blength = 10000;
-		int[] b = new int[blength];
-		for(int i=0; i<blength; i++){
-			b[i] = i;
-		}
-		LangOps.timeIt("100次", ()->{
-			for(int i=0; i<alength; i++){
-				if(a[i]==99){
-					System.out.println("finish!");
-				}
-			}
-		});
-		LangOps.timeIt("10000次", ()->{
-			for(int i=0; i<blength; i++){
-				if(b[i]==9999){
-					System.out.println("finish!");
-				}
-			}
-		});
+		String str = String.format( "(devType:%s^1.5)", 1 );
+		System.out.println("str:"+str);
+		int a = 1^5;
 	}
 
 }
