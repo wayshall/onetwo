d659e5ba3a5a9f9fe15d26491efb7c455433b6e4
wayshall <weishao.zeng@gmail.com>
2016-04-07 09:37:18 +0800

    增加内嵌tomcat模块； 和其它一些小修改

---
+++ core/modules/tomcat/src/main/java/org/onetwo/common/web/tomcatmini/Utils.java
@@ -0,0 +1,13 @@
+package org.onetwo.common.web.tomcatmini;
+
+public class Utils {
+	
+
+	public static boolean isBlank(String str) {
+		return str == null || str.trim().equals("");
+	}
+	
+	private Utils(){
+	}
+
+}
