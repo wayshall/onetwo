7f35e3fc092b924a4194665e1805ca6a573b2a4c
wayshall <weishao.zeng@gmail.com>
2016-06-12 20:15:29 +0800

    remove poi

--- core/modules/poi/src/main/java/org/onetwo/common/excel/ExtractorMapKey.java
+++
@@ -1,7 +0,0 @@
-package org.onetwo.common.excel;
-
-public interface ExtractorMapKey {
-	
-	String getExtractorMapKey();
-
-}
--- core/modules/poi/src/main/java/org/onetwo/common/excel/RowProcessor.java
+++
@@ -1,8 +0,0 @@
-package org.onetwo.common.excel;
-
-import org.onetwo.common.excel.data.RowContextData;
-
-
-public interface RowProcessor {
-	public void processRow(RowContextData rowContext);
-}
