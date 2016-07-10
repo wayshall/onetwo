29714583d137e783aa5a359f26f28e696f6a04c5
wayshall <weishao.zeng@gmail.com>
2016-07-10 23:50:07 +0800

    webadmi

---
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/controller/DefaultErrorController.java
@@ -0,0 +1,21 @@
+package org.onetwo.boot.core.web.controller;
+
+import org.onetwo.boot.plugin.web.PluginBaseController;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class DefaultErrorController extends PluginBaseController implements ErrorController {
+	private static final String PATH = "/error";
+	
+	@RequestMapping(value = PATH)
+    public ModelAndView error() {
+        return pluginMv("error");
+    }
+	 
+	@Override
+    public String getErrorPath() {
+	    return PATH;
+    }
+
+}
