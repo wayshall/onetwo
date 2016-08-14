1d26ab18aea5f4ded3192d8c34b35d0ac5fb1d21
wayshall <weishao.zeng@gmail.com>
2016-08-14 23:17:29 +0800

    修复没有entityManager时启动失败的bug 把dbm的参数设置和结果获取抽象为统一接口

---
+++ example/web-manager/src/main/java/org/onetwo/webapp/manager/controller/TestRestController.java
@@ -0,0 +1,35 @@
+package org.onetwo.webapp.manager.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.onetwo.common.exception.ServiceException;
+import org.onetwo.common.result.AbstractDataResult.SimpleDataResult;
+import org.onetwo.common.spring.web.mvc.utils.WebResultCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/rest")
+public class TestRestController {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@RequestMapping("say")
+	public SimpleDataResult<?> say(HttpServletRequest request, String word){
+		System.out.println("this.request:"+this.request);
+		System.out.println("request:"+request);
+		if("error".equals(word)){
+			throw new ServiceException("error word!");
+		}
+		return WebResultCreator.creator().simple(word).buildResult();
+	}
+	
+	@ExceptionHandler
+	public SimpleDataResult<?> exceptionHandler(Throwable e){
+		return WebResultCreator.creator().error(e.getMessage()).buildResult();
+	}
+
+}
