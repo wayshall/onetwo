2624981983e19743f6aaab342834cea13965734f
wayshall <weishao.zeng@gmail.com>
2016-10-27 18:45:57 +0800

    add spring retry to bom and empty test class

---
+++ core/modules/spring/src/test/java/org/onetwo/common/spring/retry/RetryTest.java
@@ -0,0 +1,9 @@
+package org.onetwo.common.spring.retry;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(classes=RetryTestContext.class)
+public class RetryTest extends AbstractJUnit4SpringContextTests {
+
+}
---
+++ core/modules/spring/src/test/java/org/onetwo/common/spring/retry/RetryTestContext.java
@@ -0,0 +1,5 @@
+package org.onetwo.common.spring.retry;
+
+public class RetryTestContext {
+
+}
