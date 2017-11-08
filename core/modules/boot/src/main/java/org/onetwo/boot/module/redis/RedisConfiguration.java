c0432b80cd744f21e4a0cccf2704f3d6b350a2f9
wayshall <weishao.zeng@gmail.com>
2017-11-08 10:43:04 +0800

    add condition to jfish redis

--- core/modules/boot/src/main/java/org/onetwo/boot/module/redis/RedisConfiguration.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/redis/RedisConfiguration.java
@@ -20,7 +20,6 @@ import org.springframework.integration.redis.util.RedisLockRegistry;
  */
 @EnableConfigurationProperties({JFishRedisProperties.class})
 @ConditionalOnClass({JedisConnectionFactory.class, RedisLockRegistry.class})
-@ConditionalOnProperty(name=JFishRedisProperties.ENABLED_KEY, havingValue="true")
 public class RedisConfiguration {
 	
 	private static final String BEAN_REDISCONNECTIONFACTORY = "redisConnectionFactory";
@@ -32,6 +31,7 @@ public class RedisConfiguration {
     
 	@Bean
     @ConditionalOnMissingBean(name=BEAN_REDISCONNECTIONFACTORY)
+	@ConditionalOnProperty(name=JFishRedisProperties.ENABLED_KEY, havingValue="true")
     public JedisConnectionFactory redisConnectionFactory() throws Exception {
 		JedisConnectionFactory jf = new JedisConnectionFactory();
 		CopyUtils.copy(jf, redisProperties);
@@ -42,6 +42,7 @@ public class RedisConfiguration {
     }
 	
 	@Bean
+	@ConditionalOnProperty(name=JFishRedisProperties.ENABLED_KEY, havingValue="true")
 	public RedisTemplate<String, Object> redisTemplate(@Autowired JedisConnectionFactory jedisConnectionFactory) throws Exception  {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setKeySerializer(new StringRedisSerializer());
