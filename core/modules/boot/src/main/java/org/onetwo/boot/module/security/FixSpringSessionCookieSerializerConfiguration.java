2bb5ead6a01bc478be4eeb0551f009dec9e47be7
wayshall <weishao.zeng@gmail.com>
2017-11-08 18:48:38 +0800

    fix spring session cookies path

---
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/security/FixSpringSessionCookieSerializerConfiguration.java
@@ -0,0 +1,41 @@
+package org.onetwo.boot.module.security;
+
+import org.onetwo.common.utils.StringUtils;
+import org.onetwo.ext.security.utils.SecurityConfig.CookieConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.Session;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @author wayshall
+ * <br/>
+ */
+@Configuration
+@ConditionalOnClass(Session.class)
+@EnableConfigurationProperties(BootSecurityConfig.class)
+public class FixSpringSessionCookieSerializerConfiguration {
+	
+	@Autowired
+	private BootSecurityConfig securityConfig;
+	
+	@Bean
+	@ConditionalOnMissingBean(CookieSerializer.class)
+	public CookieSerializer cookieSerializer(){
+		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+		CookieConfig cookieConfig = securityConfig.getCookie();
+		if(StringUtils.isNotBlank(cookieConfig.getPath())){
+			serializer.setCookiePath(cookieConfig.getPath());
+		}
+		if(StringUtils.isNotBlank(cookieConfig.getDomain())){
+			serializer.setDomainName(cookieConfig.getDomain());
+		}
+		return serializer;
+	}
+
+}
