4106287d6df01324245d0fc89d15faedc1a1da71
wayshall <weishao.zeng@gmail.com>
2018-01-16 17:57:09 +0800

    add simple transaction listener

---
+++ core/modules/rocketmq/src/main/java/org/onetwo/ext/ons/transaction/SpringTransactionListener.java
@@ -0,0 +1,42 @@
+package org.onetwo.ext.ons.transaction;
+
+import org.onetwo.ext.alimq.ProducerListener;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.SendResult;
+
+/**
+ * @author wayshall
+ * <br/>
+ */
+public class SpringTransactionListener implements ProducerListener<Message> {
+
+	@Override
+	public void beforeSendMessage(Message message) {
+		storeMessage(message);
+	}
+
+	@Override
+	public void afterSendMessage(Message message, SendResult sendResult) {
+		//不用处理
+	}
+
+	@Override
+	public void onSendMessageError(Message message, Throwable throable) {
+		//不用处理
+	}
+	
+	@TransactionalEventListener(phase=TransactionPhase.AFTER_COMMIT)
+	public void afterCommit(){
+		findMessage();
+		sendMessage();
+	}
+	
+	@TransactionalEventListener(phase=TransactionPhase.AFTER_ROLLBACK)
+	public void afterRollback(){
+		//不用处理
+	}
+
+}
