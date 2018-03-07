22c96a0ac1b26b437b5d7a5ddac263a2f6988cd3
wayshall <weishao.zeng@gmail.com>
2018-03-07 16:07:12 +0800

    add JmsMessage

---
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/jms/JmsMessage.java
@@ -0,0 +1,26 @@
+package org.onetwo.boot.module.jms;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author wayshall
+ * <br/>
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JmsMessage<T extends Serializable> implements Serializable {
+	/**
+	 * @author wayshall
+	 * 
+	 */
+	private static final long serialVersionUID = -8805102399544290246L;
+	String key;
+	T payload;
+
+}
