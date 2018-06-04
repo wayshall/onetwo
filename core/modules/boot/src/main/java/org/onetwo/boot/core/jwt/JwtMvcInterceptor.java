8257f4310582027e819e79af51a1ab69db9d1d16
wayshall <weishao.zeng@gmail.com>
2018-06-04 11:25:11 +0800

    fix jwt login error

--- core/modules/boot/src/main/java/org/onetwo/boot/core/jwt/JwtMvcInterceptor.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/jwt/JwtMvcInterceptor.java
@@ -31,10 +31,14 @@ public class JwtMvcInterceptor extends MvcInterceptorAdapter {
 		try {
 			userOpt = JwtUtils.getOrSetJwtUserDetail(request, jwtTokenService, authHeaderName);
 		} catch (ServiceException e) {
-			if(e.getExceptionType() instanceof JwtErrors && e.getExceptionType()!=JwtErrors.CM_NOT_LOGIN){
+			/*if(e.getExceptionType() instanceof JwtErrors && e.getExceptionType()!=JwtErrors.CM_NOT_LOGIN){
 				throw new ServiceException(JwtErrors.CM_NOT_LOGIN, e);
-			}
+			}*/
+			throw e;
+		} catch (Exception e){
+			throw new ServiceException(JwtErrors.CM_LOGIN_UNKNOW_ERR, e);
 		}
+		
 		if(!userOpt.isPresent()){
 			throw new ServiceException(JwtErrors.CM_NOT_LOGIN);
 		}
