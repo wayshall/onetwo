9f67071730db14592bd9d47d353fd1f2a0f969d7
wayshall <weishao.zeng@gmail.com>
2018-07-12 23:52:13 +0800

    fix formBodyWrapper for zuul

---
+++ core/modules/cloud/src/main/java/org/onetwo/cloud/bugfix/FixFormBodyWrapperFilterPostProcessor.java
@@ -0,0 +1,36 @@
+package org.onetwo.cloud.bugfix;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cloud.netflix.zuul.filters.pre.FormBodyWrapperFilter;
+
+/**
+ * bug相关连接：
+ * https://github.com/spring-cloud/spring-cloud-netflix/issues/1385
+ * 
+ * https://jira.spring.io/browse/SPR-15205
+ * 
+ * @author wayshall
+ * <br/>
+ */
+public class FixFormBodyWrapperFilterPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		if(bean instanceof FormBodyWrapperFilter){
+			//因为关键代码都是private，所以直接复制了spring 5.0 的两个类过来
+			AllEncompassingFormHttpMessageConverter converter = new AllEncompassingFormHttpMessageConverter();
+//			converter.setMultipartCharset(Charset.forName("utf-8"));
+			return new FormBodyWrapperFilter(converter);
+		}
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		return bean;
+	}
+
+}
