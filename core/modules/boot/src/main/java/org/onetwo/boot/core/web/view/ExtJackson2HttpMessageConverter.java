e41c797506e3baa8e38184106bfb895b138719ba
wayshall <weishao.zeng@gmail.com>
2018-07-19 17:05:43 +0800

    add AcceptCharset for json converter

--- core/modules/boot/src/main/java/org/onetwo/boot/core/web/view/ExtJackson2HttpMessageConverter.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/view/ExtJackson2HttpMessageConverter.java
@@ -1,12 +1,17 @@
 package org.onetwo.boot.core.web.view;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import org.onetwo.boot.core.json.ObjectMapperProvider;
 import org.onetwo.common.jackson.JsonMapper;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
@@ -38,6 +43,12 @@ public class ExtJackson2HttpMessageConverter extends MappingJackson2HttpMessageC
 		}
 	}
 	
+
+	@Override
+	protected void writeInternal(Object object, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+		outputMessage.getHeaders().setAcceptCharset(Arrays.asList(StandardCharsets.UTF_8));
+		super.writeInternal(object, type, outputMessage);
+	}
 
 	public void setMapperBuilder(Jackson2ObjectMapperBuilder mapperBuilder) {
 		this.mapperBuilder = mapperBuilder;
