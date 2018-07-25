afa6a1f3df8324a47008fcf0537dd528ec5c5708
wayshall <weishao.zeng@gmail.com>
2018-07-25 14:03:17 +0800

    remove unuse code

--- core/modules/spring/src/main/java/org/onetwo/common/apiclient/impl/DefaultRestExecutorFactory.java
+++ core/modules/spring/src/main/java/org/onetwo/common/apiclient/impl/DefaultRestExecutorFactory.java
@@ -2,27 +2,21 @@ package org.onetwo.common.apiclient.impl;
 
 import org.onetwo.common.apiclient.RestExecutor;
 import org.onetwo.common.apiclient.RestExecutorFactory;
-import org.onetwo.common.apiclient.utils.ApiClientUtils;
 import org.onetwo.common.spring.rest.ExtRestTemplate;
 import org.onetwo.common.spring.rest.RestUtils;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 
 /**
  * @author wayshall
  * <br/>
  */
-public class DefaultRestExecutorFactory extends RestExecutorFactory implements InitializingBean {
+public class DefaultRestExecutorFactory extends RestExecutorFactory {
 	
-	static private final Logger logger = ApiClientUtils.getApiclientlogger();
+//	static private final Logger logger = ApiClientUtils.getApiclientlogger();
 
 	@Autowired(required=false)
 	private RestExecutorConfig restExecutorConfig;
-	@Autowired
-	private ApplicationContext applicationContext;
 	
 	public DefaultRestExecutorFactory() {
 	}
@@ -46,27 +40,5 @@ public class DefaultRestExecutorFactory extends RestExecutorFactory implements I
 		}
 		return restTemplate;
 	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		/*ExtRestTemplate restTemplate = (ExtRestTemplate) getObject();
-		Map<String, Object> restExecutorInterceptors = applicationContext.getBeansWithAnnotation(RestExecutorInterceptor.class);
-		if(!LangUtils.isEmpty(restExecutorInterceptors)){
-			List<ClientHttpRequestInterceptor> interList = restTemplate.getInterceptors();
-			if(interList==null){
-				interList = Lists.newArrayList();
-				restTemplate.setInterceptors(interList);
-			}
-			for(Entry<String, Object> entry : restExecutorInterceptors.entrySet()){
-				if(logger.isDebugEnabled()){
-					logger.debug("register ClientHttpRequestInterceptor for RestExecutor: {}", entry.getKey());
-				}
-				interList.add((ClientHttpRequestInterceptor)entry.getValue());
-			}
-			AnnotationAwareOrderComparator.sort(interList);
-		}*/
-	}
-	
-	
 
 }
