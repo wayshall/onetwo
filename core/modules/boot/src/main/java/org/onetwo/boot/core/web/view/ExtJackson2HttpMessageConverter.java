0c470b16067edc2a83c04848469b572dc042ec8d
wayshall <weishao.zeng@gmail.com>
2018-08-06 15:17:57 +0800

    ExtJackson2HttpMessageConverter增加jsonp支持

--- core/modules/boot/src/main/java/org/onetwo/boot/core/web/view/ExtJackson2HttpMessageConverter.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/view/ExtJackson2HttpMessageConverter.java
@@ -3,9 +3,17 @@ package org.onetwo.boot.core.web.view;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Pattern;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.onetwo.boot.core.json.ObjectMapperProvider;
 import org.onetwo.common.jackson.JsonMapper;
+import org.onetwo.common.web.utils.WebHolder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpOutputMessage;
@@ -13,12 +21,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonValue;
 
 /**
  * @author wayshall
  * <br/>
  */
 public class ExtJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter implements InitializingBean {
+	private Set<String> jsonpParameterNames = new LinkedHashSet<String>(Arrays.asList("jsonp", "callback"));
+	private static final Pattern CALLBACK_PARAM_PATTERN = Pattern.compile("[0-9A-Za-z_\\.]*");
 	
 	private Jackson2ObjectMapperBuilder mapperBuilder;
 	@Autowired(required=false)
@@ -43,9 +54,14 @@ public class ExtJackson2HttpMessageConverter extends MappingJackson2HttpMessageC
 	}
 	
 
+	
 	@Override
 	protected void writeInternal(Object object, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
 //		outputMessage.getHeaders().setAcceptCharset(Arrays.asList(StandardCharsets.UTF_8));
+		Optional<HttpServletRequest> requestOpt = WebHolder.getRequest();
+		if(requestOpt.isPresent()){
+			object = filterAndWrapModel(object, requestOpt.get());
+		}
 		super.writeInternal(object, type, outputMessage);
 	}
 
@@ -55,6 +71,47 @@ public class ExtJackson2HttpMessageConverter extends MappingJackson2HttpMessageC
 
 	public void setObjectMapperProvider(ObjectMapperProvider objectMapperProvider) {
 		this.objectMapperProvider = objectMapperProvider;
+	}
+
+	protected Object filterAndWrapModel(Object value, HttpServletRequest request) {
+		String jsonpParameterValue = getJsonpParameterValue(request);
+		if (jsonpParameterValue != null) {
+			if (value instanceof MappingJacksonValue) {
+				((MappingJacksonValue) value).setJsonpFunction(jsonpParameterValue);
+			}
+			else {
+				MappingJacksonValue container = new MappingJacksonValue(value);
+				container.setJsonpFunction(jsonpParameterValue);
+				value = container;
+			}
+		}
+		return value;
+	}
+	
+	public void setJsonpParameterNames(Set<String> jsonpParameterNames) {
+		this.jsonpParameterNames = jsonpParameterNames;
+	}
+
+	private String getJsonpParameterValue(HttpServletRequest request) {
+		if (this.jsonpParameterNames != null) {
+			for (String name : this.jsonpParameterNames) {
+				String value = request.getParameter(name);
+				if (StringUtils.isEmpty(value)) {
+					continue;
+				}
+				if (!isValidJsonpQueryParam(value)) {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Ignoring invalid jsonp parameter value: " + value);
+					}
+					continue;
+				}
+				return value;
+			}
+		}
+		return null;
+	}
+	protected boolean isValidJsonpQueryParam(String value) {
+		return CALLBACK_PARAM_PATTERN.matcher(value).matches();
 	}
 	
 }
