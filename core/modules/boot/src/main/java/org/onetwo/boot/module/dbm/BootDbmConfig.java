925a6cdc1b62bae440e4b096a8b82195dd40fcc0
wayshall <weishao.zeng@gmail.com>
2018-08-29 22:59:29 +0800

    fix:
    change enabled key name: web.statisController.enabled

--- core/modules/boot/src/main/java/org/onetwo/boot/module/dbm/BootDbmConfig.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/dbm/BootDbmConfig.java
@@ -6,6 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix="jfish.dbm")
 public class BootDbmConfig extends DefaultDbmConfig {
 	
-	public static final String STATIS_ENABLED_KEY = "statisController.enabled";
+	public static final String STATIS_ENABLED_KEY = "web.statisController.enabled";
 
 }
