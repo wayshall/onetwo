1508dd559d220ad6d36b1f2f9d9d776e5809df8c
wayshall <weishao.zeng@gmail.com>
2018-08-29 22:48:31 +0800

    feat:
    增加配置控制dbm的统计controller

--- core/modules/boot/src/main/java/org/onetwo/boot/module/dbm/BootDbmConfiguration.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/dbm/BootDbmConfiguration.java
@@ -4,6 +4,7 @@ import org.onetwo.dbm.core.spi.DbmSession;
 import org.onetwo.dbm.mapping.DbmConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +27,7 @@ public class BootDbmConfiguration {
 	}
 	
 	@Bean
+	@ConditionalOnProperty(name=BootDbmConfig.STATIS_ENABLED_KEY, matchIfMissing=false)
 	public DbmStatisController dbmStatisController(){
 		return new DbmStatisController();
 	}
