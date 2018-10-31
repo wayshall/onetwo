27d55507525501679ac50299a049b2630f13c670
wayshall <weishao.zeng@gmail.com>
2018-10-31 15:23:39 +0800

    fix:
    fix rmq module test code

--- core/modules/rocketmq/src/test/java/org/onetwo/ext/rmqwithonsclient/producer/DataBaseProducerServiceImpl.java
+++ core/modules/rocketmq/src/test/java/org/onetwo/ext/rmqwithonsclient/producer/DataBaseProducerServiceImpl.java
@@ -51,21 +51,24 @@ public class DataBaseProducerServiceImpl {
 																	  .build());
 		System.out.println("res: " + res);
 		
-		res = onsProducerService.sendMessage(
+		SendResult sendResult = onsProducerService.sendMessage(
 				SimpleMessage.builder()
-				  .topic(RmqONSProducerTest.TOPIC)
-				  .tags(RmqONSProducerTest.ORDER_CANCEL)
-//				  .key("test_"+idGenerator.nextId())
-				  .dataId("2")
-				  .userId("100")
-				  .occurOn(new Date())
-				  .body(
+				  .topic(RmqONSProducerTest.TOPIC) // 消息主题
+				  .tags(RmqONSProducerTest.ORDER_CANCEL) // 消息标签
+				  .userId("100") // 表示订单用户
+				  .dataId("2") // 表示订单id
+				  .occurOn(new Date()) //消息发送时间，可不写，默认为当前
+				  // body以外的属性属于发送时需要的属性，比如上面三个属性主要是用于生成消息的唯一key，
+				  // 如果自己有设置key属性，则不需要；这些属性暂时不会发送到客户端
+				  .body( // 这里才是实际要发送的，客户端收到的消息体
 						  OrderTestMessage.builder()
-			  				.orderId(2L)
-			  				.title("取消")
-			  				.build()
-				  ).build(), SendMessageFlags.EnableDatabaseTransactional);
-		System.out.println("res: " + res);
+							  				.orderId(2L)
+							  				.title("取消")
+							  				.build()
+				  ).build(), 
+				  SendMessageFlags.EnableDatabaseTransactional);// 启动消息本地事务，以保持一致性
+		
+		System.out.println("sendResult: " + sendResult);
 	}
 
 	public void sendMessageWithException(){
