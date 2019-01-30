4233aebececaaf088f9f88de3e517a3e3dc29900
wayshall <weishao.zeng@gmail.com>
2019-01-30 10:10:36 +0800

    add rmq interceptor

--- core/modules/cloud/src/main/java/org/onetwo/cloud/sleuth/SleuthConfiguration.java
+++ core/modules/cloud/src/main/java/org/onetwo/cloud/sleuth/SleuthConfiguration.java
@@ -1,5 +1,6 @@
 package org.onetwo.cloud.sleuth;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,13 @@ public class SleuthConfiguration {
 	public SleuthDbmInterceptor sleuthDbmInterceptor(){
 		SleuthDbmInterceptor interceptor = new SleuthDbmInterceptor();
 		return interceptor;
+	}
+	
+	@Bean
+	@ConditionalOnClass(name="org.onetwo.ext.ons.producer.ONSProducerServiceImpl")
+	@ConditionalOnBean(type="org.onetwo.ext.ons.producer.ONSProducerServiceImpl")
+	public SleuthRocketmqInterceptor SleuthRocketmqInterceptor() {
+		return new SleuthRocketmqInterceptor();
 	}
 
 }
