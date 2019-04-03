160b3f7e812a8d974ed6cb2a2edf30355a2e0bd1
wayshall <weishao.zeng@gmail.com>
2019-04-03 23:39:15 +0800

    fix: to-merge: add RedisOperationService interface

---
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/redis/RedisOperationService.java
@@ -0,0 +1,38 @@
+package org.onetwo.boot.module.redis;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * @author weishao zeng
+ * <br/>
+ */
+public interface RedisOperationService {
+
+	RedisLockRunner getRedisLockRunnerByKey(String key);
+
+	<T> Optional<T> getCacheIfPreset(String key, Class<T> clazz);
+
+	/***
+	 * 根据key获取缓存，如果还不存在，则上锁执行cacheLoader
+	 * @author weishao zeng
+	 * @param key
+	 * @param cacheLoader
+	 * @return
+	 */
+	<T> T getCache(String key, Supplier<CacheData<T>> cacheLoader);
+
+	String getAndDel(String key);
+
+	Long clear(String key);
+
+	RedisTemplate<Object, Object> getRedisTemplate();
+
+	StringRedisTemplate getStringRedisTemplate();
+
+	CacheStatis getCacheStatis();
+
+}
