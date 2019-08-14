dec704d082033bd603f4045f00f32d84c1ddd0ab
wayshall <weishao.zeng@gmail.com>
2019-08-14 21:16:26 +0800

    fix eureka

--- core/modules/cloud/src/main/java/org/onetwo/cloud/eureka/EurekaClientContextConfig.java
+++ core/modules/cloud/src/main/java/org/onetwo/cloud/eureka/EurekaClientContextConfig.java
@@ -3,6 +3,7 @@ package org.onetwo.cloud.eureka;
 import org.onetwo.boot.core.init.BootServletContextInitializer;
 import org.onetwo.cloud.core.BootJfishCloudConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @EnableDiscoveryClient
 @EnableConfigurationProperties({BootJfishCloudConfig.class})
 @Configuration
+@ConditionalOnProperty(name="eureka.client.enabled", matchIfMissing=true)
 public class EurekaClientContextConfig {
 
 	@Bean
-	@ConditionalOnBean(BootServletContextInitializer.class)
+	@ConditionalOnBean({BootServletContextInitializer.class})
 	public EurekaUnregisterProcessor eurekaUnregisterProcessor() {
 		return new EurekaUnregisterProcessor();
 	}
