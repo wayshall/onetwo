47da22a42f507517496495313826995caf631ecf
wayshall <weishao.zeng@gmail.com>
2019-10-31 11:34:03 +0800

    fix producer id

--- core/modules/rocketmq/src/main/java/org/onetwo/ext/ons/producer/ProducerRegistar.java
+++ core/modules/rocketmq/src/main/java/org/onetwo/ext/ons/producer/ProducerRegistar.java
@@ -2,7 +2,6 @@ package org.onetwo.ext.ons.producer;
 
 import org.onetwo.common.spring.context.BaseImportRegistrar;
 import org.onetwo.common.utils.LangUtils;
-import org.onetwo.common.utils.StringUtils;
 import org.onetwo.ext.ons.ONSProperties;
 import org.onetwo.ext.ons.annotation.EnableONSClient;
 import org.onetwo.ext.ons.producer.ONSTransactionProducerServiceImpl.FakeProducerService;
@@ -31,7 +30,7 @@ public class ProducerRegistar extends BaseImportRegistrar<EnableONSClient> {
 		for(AnnotationAttributes producer : producers){
 			String producerId = resolveAttribute(producer, "producerId", null);
 //			String beanName = "ONS-"+producerId;
-			String beanName = StringUtils.appendStartWith(producerId, "ons-");
+			String beanName = producerId;//StringUtils.appendStartWith(producerId, "ons-");
 			
 			if(registry.containsBeanDefinition(beanName)){
 				logger.info("produer[{}] has been registered, ignored...", beanName);
