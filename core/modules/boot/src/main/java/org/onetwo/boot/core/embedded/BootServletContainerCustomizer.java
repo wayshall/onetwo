d6828380b0b781422bd9143cb19fa1140a5e52d7
wayshall <weishao.zeng@gmail.com>
2020-09-28 00:42:03 +0800

    fix apr mode

--- core/modules/boot/src/main/java/org/onetwo/boot/core/embedded/BootServletContainerCustomizer.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/embedded/BootServletContainerCustomizer.java
@@ -1,7 +1,7 @@
 package org.onetwo.boot.core.embedded;
 
 import org.apache.catalina.core.AprLifecycleListener;
-import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.onetwo.common.file.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.MultipartProperties;
@@ -41,12 +41,13 @@ public class BootServletContainerCustomizer implements EmbeddedServletContainerC
             if (tomcatProperties.isAprProtocol()) {
             	tomcat.setProtocol("org.apache.coyote.http11.Http11AprProtocol");
             	tomcat.addContextLifecycleListeners(new AprLifecycleListener());
+            	
             }
             tomcat.addConnectorCustomizers(
                 (connector) -> {
                 	//connector 本身默认是 2 mb
                 	connector.setMaxPostSize(FileUtils.parseSize(multipartProperties.getMaxRequestSize()));
-                	Http11NioProtocol handler = (Http11NioProtocol)connector.getProtocolHandler();
+                	AbstractHttp11Protocol<?> handler = (AbstractHttp11Protocol<?>)connector.getProtocolHandler();
                 	if(tomcatProperties.getBacklog()!=-1){
                 		//socket 连接队列大小
 //                		handler.setBacklog(tomcatProperties.getBacklog());
