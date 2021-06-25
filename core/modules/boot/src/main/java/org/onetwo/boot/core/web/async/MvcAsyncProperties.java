ef674e40ee11ad9c1cba5ab35a22a88388b9ea99
weishao zeng <weishao.zeng@gmail.com>
2021-06-25 19:30:29 +0800

    fix mvc async config

--- core/modules/boot/src/main/java/org/onetwo/boot/core/web/async/MvcAsyncProperties.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/async/MvcAsyncProperties.java
@@ -5,6 +5,11 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * 
+jfish:
+	mvc: 
+		async:
+			timeout: 30000
  * @author wayshall
  * <br/>
  */
@@ -14,7 +19,7 @@ public class MvcAsyncProperties {
 	public static final String PREFIX = org.onetwo.boot.core.config.BootJFishConfig.ZIFISH_CONFIG_PREFIX+ ".mvc.async";
 	public static final String ENABLE_KEY = PREFIX+".enabled";
 
-	private int timeout = 60000;
+	private int timeout = 30_000;
 	private int corePoolSize = 5;
 	private int maxPoolSize = 50;
 	private int keepAliveSeconds = 60;
