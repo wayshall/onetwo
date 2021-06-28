e695e420b1e06a17d477012627a9c5a261598edc
weishao zeng <weishao.zeng@gmail.com>
2021-06-28 14:23:27 +0800

    fix: fix mvc aysnc default timeout

--- core/modules/boot/src/main/java/org/onetwo/boot/core/web/async/MvcAsyncProperties.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/core/web/async/MvcAsyncProperties.java
@@ -19,7 +19,7 @@ public class MvcAsyncProperties {
 	public static final String PREFIX = org.onetwo.boot.core.config.BootJFishConfig.ZIFISH_CONFIG_PREFIX+ ".mvc.async";
 	public static final String ENABLE_KEY = PREFIX+".enabled";
 
-	private int timeout = 30_000;
+	private int timeout = 60_000;
 	private int corePoolSize = 5;
 	private int maxPoolSize = 50;
 	private int keepAliveSeconds = 60;
