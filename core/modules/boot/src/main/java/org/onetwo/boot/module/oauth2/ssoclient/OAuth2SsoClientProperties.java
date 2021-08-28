6555174e4fbd4cfee271d0da2c629bb36d949ff8
weishao zeng <weishao.zeng@gmail.com>
2021-08-28 13:59:47 +0800

    doc: add comment for oauth sso client

--- core/modules/boot/src/main/java/org/onetwo/boot/module/oauth2/ssoclient/OAuth2SsoClientProperties.java
+++ core/modules/boot/src/main/java/org/onetwo/boot/module/oauth2/ssoclient/OAuth2SsoClientProperties.java
@@ -27,6 +27,15 @@ public class OAuth2SsoClientProperties {
 	
 	RestExecutorConfig resttemplate = new RestExecutorConfig(60_000, 60_000, 60_000);
 	
+	/***
+	 * 是否强制检查state参数。
+	 * 
+	 * 这里注意：
+	 * 	本地测试，如果后端服务和前端分开时，两者使用不同的域名和端口，或者设置不同的context-path时，
+	 * 因为DefaultOAuth2ClientContext是session scope的bean，设置state参数和使用state和code参数获取accessToken时其实是不同的会话（因为域名等不同导致了cookies不同），
+	 * 即由于验证前和验证后重定向回来时无法保持同一个会话的bean，导致无法获取PreservedState，从而导致csrf异常。
+	 * 所以如果前后端域名不同，应该设置stateMandatory=false，便面csrf异常
+	 */
 	boolean stateMandatory = true;
 	
 	String loginPath = "/oauth2Login*";
