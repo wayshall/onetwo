302c553ffa1a11c618a61a91c897baa319c1e9af
weishao zeng <weishao.zeng@gmail.com>
2023-05-05 10:10:59 +0800

    fix: eureka add config

--- core/modules/cloud/src/main/java/org/onetwo/cloud/eureka/server/EnableJFishEurekaServerExtensionSelector.java
+++ core/modules/cloud/src/main/java/org/onetwo/cloud/eureka/server/EnableJFishEurekaServerExtensionSelector.java
@@ -3,6 +3,7 @@ package org.onetwo.cloud.eureka.server;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.onetwo.boot.core.web.mvc.ErrorHandleConfiguration;
 import org.onetwo.common.spring.context.AbstractImportSelector;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
@@ -17,7 +18,8 @@ public class EnableJFishEurekaServerExtensionSelector extends AbstractImportSele
 	@Override
 	protected List<String> doSelect(AnnotationMetadata metadata, AnnotationAttributes attributes) {
 		List<String> classNames = new ArrayList<String>();
-		
+
+		classNames.add(ErrorHandleConfiguration.class.getName());
 		classNames.add(ExtEurekaServerConfiguration.class.getName());
 		
 		return classNames;
