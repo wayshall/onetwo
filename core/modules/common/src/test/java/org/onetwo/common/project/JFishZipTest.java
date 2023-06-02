5793bb6a607342e26be3aaaeb6be432403814626
weishao zeng <weishao.zeng@gmail.com>
2023-06-02 17:01:14 +0800

    fix: add source code print

--- core/modules/common/src/test/java/org/onetwo/common/project/JFishZipTest.java
+++ core/modules/common/src/test/java/org/onetwo/common/project/JFishZipTest.java
@@ -1,10 +1,6 @@
 package org.onetwo.common.project;
 
-import java.io.FileNotFoundException;
-
 import org.junit.Test;
-import org.onetwo.common.file.BufferedWriterWrapper;
-import org.onetwo.common.file.FileUtils;
 
 /**
  * @author wayshall
@@ -17,7 +13,7 @@ public class JFishZipTest {
 		String dir = "D:\\mydev\\java\\basedir";
 		
 		ProjectRefactor refactor = new ProjectRefactor(dir);
-		refactor.newZipFile("C:\\Users\\way\\Desktop\\data\\tbc-sources.zip")
+		refactor.newZipFile("C:\\Users\\way\\Desktop\\data\\java-sources.zip")
 					.andSubDirIs("project1 project2", " ")
 					.andPostfixIsAnyOf(".java")
 					.andIgnoreDirContains("src/test", "target")
@@ -25,39 +21,4 @@ public class JFishZipTest {
 				.execute();
 	}
 	
-
-	
-	@Test
-	public void testPrintJavaFiles() throws FileNotFoundException{
-		String dir = "D:\\mydev\\java\\basedir";
-		
-		ProjectRefactor refactor = new ProjectRefactor(dir);
-		
-		BufferedWriterWrapper bw = FileUtils.createOrGetBufferedWriter("C:\\Users\\way\\Desktop\\data\\test.md");
-		
-		FileProccessExecutor executor = (baseDir, file) -> {
-			if (bw.getLineCount()>1500) {
-				return false;
-			}
-			bw.writeLine("## " + file.getName());
-			bw.writeLine("```Java");
-			FileUtils.readFile(file, (line, index) -> {
-				bw.writeLine(line);
-				return true;
-			});
-			bw.writeLine("```");
-			return true;
-		};
-		// 生成markdown目录
-//		bw.writeLine("[toc]");
-		refactor.newExecutor(executor)
-		.andSubDirIs("project1 project2", " ")
-		.andPostfixIsAnyOf(".java")
-		.andIgnoreDirContains("src/test")
-		.end()
-		.execute();
-		bw.closeQuietly();
-	}
-	
-
 }
