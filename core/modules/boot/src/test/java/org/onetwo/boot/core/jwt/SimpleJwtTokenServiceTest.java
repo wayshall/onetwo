e22da2b1c0d598c743a80914ccfb18b277418a99
weishao zeng <weishao.zeng@gmail.com>
2023-10-12 11:59:32 +0800

    fix: add test

--- core/modules/boot/src/test/java/org/onetwo/boot/core/jwt/SimpleJwtTokenServiceTest.java
+++ core/modules/boot/src/test/java/org/onetwo/boot/core/jwt/SimpleJwtTokenServiceTest.java
@@ -2,8 +2,20 @@ package org.onetwo.boot.core.jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.junit.Test;
+import org.onetwo.common.date.NiceDate;
+import org.onetwo.common.jackson.JsonMapper;
+import org.onetwo.common.spring.SpringUtils;
+import org.onetwo.common.utils.LangUtils;
 import org.onetwo.common.web.userdetails.SimpleUserDetail;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
 
 /**
  * @author wayshall
@@ -32,6 +44,43 @@ public class SimpleJwtTokenServiceTest {
 		assertThat(ud.getUserId()).isEqualTo(userDetail.getUserId());
 		assertThat(ud.getUserName()).isEqualTo(userDetail.getUserName());
 //		assertThat(ud.getToken()).isEqualTo(userDetail.getToken());
+	}
+	
+	@Test
+	public void testZhipuToken() {
+		// bigmodel token
+		ApiAuthParams params = new ApiAuthParams();
+		params.setApi_key("id");
+		Date ts = NiceDate.Now().getTime();
+		Date exp = NiceDate.Now().nextMonth(1).getTime();
+		
+		params.setExp(exp.getTime());
+		params.setTimestamp(ts.getTime());
+		
+		String json = JsonMapper.toJsonString(params);
+		System.out.println("json: " + json);
+		
+		Map<String, Object> map = SpringUtils.toMap(params);
+		
+		
+		JwtBuilder builder = Jwts.builder()
+				.setClaims(map)
+				.setIssuedAt(ts)
+				.setExpiration(exp)
+				.setHeaderParam("alg", "HS256")
+				.setHeaderParam("sign_type", "SIGN")
+				.signWith(SignatureAlgorithm.HS256, LangUtils.getBytes("secrect"));
+
+		
+		String token = builder.compact();
+		System.out.println("token: " + token);
+	}
+	
+	@Data
+	public static class ApiAuthParams {
+		String api_key;
+		Long exp;
+		Long timestamp;
 	}
 
 }
